001900452ec20c0988fd1f5f40fca75a72a3defe
Mo Amini <mo.amini@nav.no>
2019-04-02 11:46:01 +0200

    forsøker å hente kafka property

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/consumer/kafka/SedConsumer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/consumer/kafka/SedConsumer.kt
@@ -2,7 +2,6 @@ package no.nav.eessi.pensjon.journalforing.consumer.kafka;
 
 import no.nav.eessi.pensjon.journalforing.integration.model.SedHendelse
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment
@@ -12,16 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 class SedConsumer : KafkaConsumer {
 
-    @Value("\${kafka.sedSendt.topic}")
-    lateinit var topic: String
-
-    @Value("\${kafka.sedSendt.groupid}")
-    lateinit var groupid: String
-
-
     private val logger = LoggerFactory.getLogger(SedConsumer::class.java)
 
-    @KafkaListener(topics = ["\${topic}"], groupId = "\${groupid}")
+    @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     override fun consume(hendelse: SedHendelse, headers: MessageHeaders, acknowledgment: Acknowledgment) {
         logger.info(hendelse.toString())
     }
