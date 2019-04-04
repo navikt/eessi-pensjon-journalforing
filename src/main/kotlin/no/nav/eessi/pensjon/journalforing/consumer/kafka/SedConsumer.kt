f18bf411722013f824d0bd68f32ea3d1368e179c
Mo Amini <mo.amini@nav.no>
2019-04-04 11:46:13 +0200

    Fikses konfigurasjon for å støtte manuell commit

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/consumer/kafka/SedConsumer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/consumer/kafka/SedConsumer.kt
@@ -1,21 +1,27 @@
 package no.nav.eessi.pensjon.journalforing.consumer.kafka;
 
+import no.nav.eessi.pensjon.journalforing.integration.model.SedHendelse
+import org.codehaus.jackson.map.ObjectMapper
 import org.slf4j.LoggerFactory
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
 class SedConsumer : KafkaConsumer {
 
     private val logger = LoggerFactory.getLogger(SedConsumer::class.java)
+    private val mapper = ObjectMapper()
 
     @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     override fun consume(hendelse: String, acknowledgment: Acknowledgment) {
-        if(hendelse.contains("P_BUC")){
-            logger.info(hendelse)
+        logger.info("Innkommet hendelse")
+        val sedHendelse = mapper.readValue(hendelse, SedHendelse::class.java)
+
+        if(sedHendelse.sektorKode == "P"){
+            logger.info("Gjelder pensjon: $sedHendelse")
+            acknowledgment.acknowledge()
         }
     }
 }
