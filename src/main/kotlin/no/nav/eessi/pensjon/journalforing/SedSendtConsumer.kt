f2bbe9011415ef5c5c28722b511b449f54cca67d
Øyvind Buer <oyvind.buer@nav.no>
2019-04-26 14:52:08 +0200

    Test innsending av melding på topic

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/SedSendtConsumer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/SedSendtConsumer.kt
@@ -1,12 +1,10 @@
 package no.nav.eessi.pensjon.journalforing
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
-
 
 @Service
 class SedConsumer {
@@ -16,13 +14,12 @@ class SedConsumer {
 
     @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     fun consume(hendelse: String, acknowledgment: Acknowledgment) {
-        logger.info("Innkommet hendelse")
-  //      val sedHendelse = mapper.readValue(hendelse, String::class.java)
-   //     System.out.println("Sedhendelse: $sedHendelse")
+        logger.info("Innkommet hendelse.")
+        val sedHendelse = mapper.readValue(hendelse, SedHendelse::class.java)
 
-//        if(sedHendelse.sektorKode.equals("P")){
-//            logger.info("Gjelder pensjon: $sedHendelse")
-//            acknowledgment.acknowledge()
-//        }
+        if(sedHendelse.sektorKode.equals("P")){
+            logger.info("Gjelder pensjon: ${sedHendelse.sektorKode}")
+            acknowledgment.acknowledge()
+        }
     }
 }
--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/EessiPensjonJournalforingApplicationTests.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/EessiPensjonJournalforingApplicationTests.kt
@@ -1,15 +1,15 @@
 package no.nav.eessi.pensjon.journalforing
 
+import org.junit.ClassRule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.test.utils.KafkaTestUtils
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule
-import org.junit.ClassRule
+import org.springframework.kafka.test.utils.KafkaTestUtils
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
 
 
 @RunWith(SpringRunner::class)
@@ -21,9 +21,8 @@ class EessiPensjonJournalforingApplicationTests {
 	fun contextLoads() {
 	}
 
-
 	companion object {
-		val SED_SENDT_TOPIC = "eessi-basis-sedSendt-v1"
+		const val SED_SENDT_TOPIC = "eessi-basis-sedSendt-v1"
 
 		@ClassRule @JvmField
 		var embeddedKafka = EmbeddedKafkaRule(1, true, SED_SENDT_TOPIC)
@@ -31,15 +30,13 @@ class EessiPensjonJournalforingApplicationTests {
 
 	@Test
 	@Throws(Exception::class)
-	fun `Send en melding på topic`() {
-		Thread.sleep(10000)
-
+	fun `Send en melding pa topic`() {
 		val senderProps = KafkaTestUtils.senderProps(embeddedKafka.embeddedKafka.brokersAsString)
 		val pf = DefaultKafkaProducerFactory<Int, String>(senderProps)
 		val template = KafkaTemplate(pf)
 		template.defaultTopic = SED_SENDT_TOPIC
-		template.sendDefault("foo")
-		template.sendDefault(0, 2, "bar")
-		template.send(SED_SENDT_TOPIC, 0, 2, "baz")
+		template.sendDefault("{ \"id\" : 1, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
+		template.sendDefault(0, 2, "{ \"id\" : 2, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
+		template.send(SED_SENDT_TOPIC, 0, 2, "{ \"id\" : 3, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_03\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
 	}
 }
