ef78d9666da82a588fd714f27d300c138f10a685
Mo Amini <mo.amini@nav.no>
2019-05-06 07:40:15 +0200

    fikset formatering

--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/EessiPensjonJournalforingApplicationTests.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/EessiPensjonJournalforingApplicationTests.kt
@@ -26,76 +26,76 @@ import javax.ws.rs.HttpMethod
 class EessiPensjonJournalforingApplicationTests {
 
 
-	companion object {
-		const val SED_SENDT_TOPIC = "eessi-basis-sedSendt-v1"
+    companion object {
+        const val SED_SENDT_TOPIC = "eessi-basis-sedSendt-v1"
 
-		@ClassRule @JvmField
-		// Start kafka in memory
-		var embeddedKafka = EmbeddedKafkaRule(1, true, SED_SENDT_TOPIC)
+        @ClassRule
+        @JvmField
+        // Start kafka in memory
+        var embeddedKafka = EmbeddedKafkaRule(1, true, SED_SENDT_TOPIC)
 
 
-		init {
-			// Start Mockserver in memory
-			val port = randomFrom()
-			val mockServer = ClientAndServer.startClientAndServer(port)
-			System.setProperty("mockServerport", port.toString())
+        init {
+            // Start Mockserver in memory
+            val port = randomFrom()
+            val mockServer = ClientAndServer.startClientAndServer(port)
+            System.setProperty("mockServerport", port.toString())
 
             // Mocker STS
-			mockServer.`when`(
-					request()
-							.withMethod(HttpMethod.GET)
-							//.withPath("/")
-							.withQueryStringParameter("grant_type", "client_credentials")
-			)
-					.respond(
-							response()
-									.withHeader(Header("Content-Type", "application/json; charset=utf-8"))
-									.withStatusCode(HttpStatusCode.OK_200.code())
-									.withBody("{\"access_token\": \"sometoken\" , \"token_type\": \"sometype\", \"expires_in\": \"123456789\"}")
-					)
+            mockServer.`when`(
+                    request()
+                            .withMethod(HttpMethod.GET)
+                            //.withPath("/")
+                            .withQueryStringParameter("grant_type", "client_credentials")
+            )
+                    .respond(
+                            response()
+                                    .withHeader(Header("Content-Type", "application/json; charset=utf-8"))
+                                    .withStatusCode(HttpStatusCode.OK_200.code())
+                                    .withBody("{\"access_token\": \"sometoken\" , \"token_type\": \"sometype\", \"expires_in\": \"123456789\"}")
+                    )
             // Mocker Eux PDF generator
-			mockServer.`when`(
-					request()
-							.withMethod(HttpMethod.GET)
-							.withPath("/buc/123/sed/123/pdf")
-			)
-					.respond(
-							response()
-									.withHeader(Header("Content-Type", "application/json; charset=utf-8"))
-									.withStatusCode(HttpStatusCode.OK_200.code())
-									.withBody("thePdf")
-					)
+            mockServer.`when`(
+                    request()
+                            .withMethod(HttpMethod.GET)
+                            .withPath("/buc/123/sed/123/pdf")
+            )
+                    .respond(
+                            response()
+                                    .withHeader(Header("Content-Type", "application/json; charset=utf-8"))
+                                    .withStatusCode(HttpStatusCode.OK_200.code())
+                                    .withBody("thePdf")
+                    )
 
-		}
+        }
 
-		fun randomFrom(from: Int = 1024, to: Int = 65535) : Int {
-			val random = Random()
-			return random.nextInt(to - from) + from
-		}
-	}
-
-
-	@Test
-	fun contextLoads() {
-	}
-
-	@Test
-	@Throws(Exception::class)
-	fun `Send en melding pa topic`() {
-		// Set up kafka
-		val senderProps = KafkaTestUtils.senderProps(embeddedKafka.embeddedKafka.brokersAsString)
-		val pf = DefaultKafkaProducerFactory<Int, String>(senderProps)
-		val template = KafkaTemplate(pf)
-		template.defaultTopic = SED_SENDT_TOPIC
-
-		// Send to kafka
-		template.sendDefault("{ \"id\" : 1, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
-		template.sendDefault(0, 2, "{ \"id\" : 2, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
-		template.send(SED_SENDT_TOPIC, 0, 2, "{ \"id\" : 3, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_03\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
-	}
+        fun randomFrom(from: Int = 1024, to: Int = 65535): Int {
+            val random = Random()
+            return random.nextInt(to - from) + from
+        }
+    }
 
 
+    @Test
+    fun contextLoads() {
+    }
 
-	fun initMockserver() {
-	}
+    @Test
+    @Throws(Exception::class)
+    fun `Send en melding pa topic`() {
+        // Set up kafka
+        val senderProps = KafkaTestUtils.senderProps(embeddedKafka.embeddedKafka.brokersAsString)
+        val pf = DefaultKafkaProducerFactory<Int, String>(senderProps)
+        val template = KafkaTemplate(pf)
+        template.defaultTopic = SED_SENDT_TOPIC
+
+        // Send to kafka
+        template.sendDefault("{ \"id\" : 1, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
+        template.sendDefault(0, 2, "{ \"id\" : 2, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_01\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
+        template.send(SED_SENDT_TOPIC, 0, 2, "{ \"id\" : 3, \"sedId\" : \"someSedId\", \"sektorKode\" : \"P\", \"bucType\" : \"FB_BUC_03\", \"rinaSakId\" : \"123\", \"avsenderId\" : \"avsenderid\", \"avsenderNavn\" : \"avsendernavn\", \"mottakerId\" : \"mottakerid\", \"mottakerNavn\" : \"mottakernavn\", \"rinaDokumentId\" : \"123\", \"rinaDokumentVersjon\" : \"1\", \"sedType\" : \"F001\", \"navBruker\" : \"12345678910\" }")
+    }
+
+
+    fun initMockserver() {
+    }
 }
