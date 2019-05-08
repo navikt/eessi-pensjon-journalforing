d5a069ed65a1aaf9a135150b1e6ccdf3cf72e117
Mo Amini <mo.amini@nav.no>
2019-05-08 08:34:17 +0200

    lagt til kall til opprett oppgave

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/kafka/SedSendtConsumer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/kafka/SedSendtConsumer.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.eessi.pensjon.journalforing.services.aktoerregister.AktoerregisterService
 import no.nav.eessi.pensjon.journalforing.services.eux.PdfService
 import no.nav.eessi.pensjon.journalforing.services.journalpost.JournalpostService
+import no.nav.eessi.pensjon.journalforing.services.oppgave.Oppgave
+import no.nav.eessi.pensjon.journalforing.services.oppgave.OppgaveService
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service
 @Service
 class SedConsumer(val pdfService: PdfService,
                   val journalpostService: JournalpostService,
+                  val oppgaveService: OppgaveService,
                   val aktoerregisterService: AktoerregisterService) {
 
     private val logger = LoggerFactory.getLogger(SedConsumer::class.java)
@@ -31,7 +34,30 @@ class SedConsumer(val pdfService: PdfService,
              //   aktoerId = aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(sedHendelse.navBruker)
             }
             journalpostService.opprettJournalpost(sedHendelse = sedHendelse, pdfBody= pdfBody!!, forsokFerdigstill = false)
+            oppgaveService.opprettOppgave(populerOppgaveFelter(sedHendelse))
             // acknowledgment.acknowledge()
         }
+    }
+
+    fun populerOppgaveFelter(sedHendelse: SedHendelse) : Oppgave {
+        val oppgave = Oppgave()
+        oppgave.oppgavetype = Oppgave.OppgaveType.JOURNALFORING.name
+
+        if(sedHendelse.bucType.equals("P_BUC_03")) {
+            oppgave.tema = Oppgave.Tema.UFORETRYGD.name
+            oppgave.behandlingstema = Oppgave.Behandlingstema.UFORE_UTLAND.name
+            oppgave.temagruppe = Oppgave.Temagruppe.UFORETRYDG.name
+
+        } else {
+           oppgave.tema = Oppgave.Tema.PENSJON.name
+           oppgave.behandlingstema = Oppgave.Behandlingstema.UTLAND.name
+           oppgave.temagruppe = Oppgave.Temagruppe.PENSJON.name
+
+        }
+        oppgave.behandlingstype = Oppgave.Behandlingstype.UTLAND.name
+        oppgave.journalpostId = "1234"
+
+
+        return oppgave
     }
 }
