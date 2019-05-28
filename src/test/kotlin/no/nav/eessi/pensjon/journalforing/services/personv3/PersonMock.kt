19af5124659642807d44dbbf2b9be669a3102807
erlend-axelsson <32471637+erlend-axelsson@users.noreply.github.com>
2019-05-28 13:51:35 +0200

    Feature/person v3 (#21)

---
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/services/personv3/PersonMock.kt
@@ -0,0 +1,19 @@
+package no.nav.eessi.pensjon.journalforing.services.personv3
+
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.*
+
+object PersonMock {
+    internal fun createWith(fodselsnr: String, landkoder: Boolean = true): Person? =
+            Person().withPersonnavn(Personnavn().withEtternavn("Testesen").withFornavn("Test"))
+                    .withKjoenn(Kjoenn().withKjoenn(Kjoennstyper().withValue("M")))
+                    .withStatsborgerskap(Statsborgerskap().withLand(Landkoder().withValue("NOR")))
+                    .withBostedsadresse(Bostedsadresse()
+                            .withStrukturertAdresse(Gateadresse()
+                                            .withGatenavn("Oppoverbakken")
+                                            .withHusnummer(66)
+                                            .withPoststed(Postnummer().withValue("1920"))
+                                            .withLandkode(when(landkoder){
+                                                        true -> Landkoder().withValue("NOR")
+                                                        else -> null
+                                            })))
+}
---
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/services/personv3/PersonV3ServiceTest.kt
@@ -0,0 +1,118 @@
+package no.nav.eessi.pensjon.journalforing.services.personv3
+
+import com.nhaarman.mockito_kotlin.doNothing
+import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockito_kotlin.mock
+import no.nav.eessi.pensjon.journalforing.models.PersonV3IkkeFunnetException
+import no.nav.eessi.pensjon.journalforing.models.PersonV3SikkerhetsbegrensningException
+import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonPersonIkkeFunnet
+import no.nav.tjeneste.virksomhet.person.v3.binding.HentPersonSikkerhetsbegrensning
+import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
+import no.nav.tjeneste.virksomhet.person.v3.feil.PersonIkkeFunnet
+import no.nav.tjeneste.virksomhet.person.v3.feil.Sikkerhetsbegrensning
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.Informasjonsbehov
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.NorskIdent
+import no.nav.tjeneste.virksomhet.person.v3.informasjon.PersonIdent
+import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonRequest
+import no.nav.tjeneste.virksomhet.person.v3.meldinger.HentPersonResponse
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+import kotlin.test.assertEquals
+
+class PersonV3ServiceTest {
+
+    lateinit var personV3 : PersonV3
+
+    lateinit var personV3Service : PersonV3Service
+
+    private val subject = "23037329381"
+    private val ikkeFunnetSubject = "33037329381"
+    private val sikkerhetsbegrensingSubject = "43037329381"
+
+    @Before
+    fun setup() {
+        personV3 = mock()
+        personV3Service = Mockito.spy(PersonV3Service(personV3))
+
+        doNothing().whenever(personV3Service).konfigurerSamlToken()
+        whenever(
+                personV3.hentPerson(requestBuilder(subject, listOf(Informasjonsbehov.ADRESSE)))
+        ).thenReturn(HentPersonResponse().withPerson(PersonMock.createWith(subject)))
+
+        whenever(
+                personV3.hentPerson(requestBuilder(ikkeFunnetSubject, listOf(Informasjonsbehov.ADRESSE)))
+        ).thenThrow(
+                HentPersonPersonIkkeFunnet(
+                        "$ikkeFunnetSubject ikke funnet",
+                        PersonIkkeFunnet()
+                )
+        )
+
+        whenever(
+                personV3.hentPerson(requestBuilder(sikkerhetsbegrensingSubject, listOf(Informasjonsbehov.ADRESSE)))
+        ).thenThrow(
+                HentPersonSikkerhetsbegrensning(
+                        "$sikkerhetsbegrensingSubject har sikkerhetsbegrensning",
+                        Sikkerhetsbegrensning()
+                )
+        )
+    }
+
+    @Test
+    fun `Kaller hentPerson med gyldig subject`(){
+        try {
+            assertEquals(personV3Service.hentPerson(subject), PersonMock.createWith(subject))
+        }catch(ex: Exception){
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Kaller hentPerson med subject som ikke finnes`(){
+        try {
+            personV3Service.hentPerson(ikkeFunnetSubject)
+            assert(false)
+        }catch(ex: Exception){
+            assert(ex is PersonV3IkkeFunnetException)
+            assertEquals(ex.message, "$ikkeFunnetSubject ikke funnet")
+        }
+    }
+
+    @Test
+    fun `Kaller hentPerson med subject med sikkerhetsbegrensing`(){
+        try {
+            personV3Service.hentPerson(sikkerhetsbegrensingSubject)
+            assert(false)
+        }catch(ex: Exception){
+            assert(ex is PersonV3SikkerhetsbegrensningException)
+            assertEquals(ex.message, "$sikkerhetsbegrensingSubject har sikkerhetsbegrensning")
+        }
+    }
+
+    @Test
+    fun `Kaller hentLandKode med gyldig subject`(){
+        try {
+            assertEquals("NOR", personV3Service.hentLandKode(PersonMock.createWith(subject)!!))
+        }catch(ex: Exception){
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Kaller hentLandKode med subject uten landkode`(){
+        try {
+            assertEquals(null,  personV3Service.hentLandKode(PersonMock.createWith(subject, false)!!))
+        }catch(ex: Exception){
+            assert(false)
+        }
+    }
+
+    fun requestBuilder(norskIdent: String, informasjonsbehov: List<Informasjonsbehov>): HentPersonRequest{
+        return HentPersonRequest().apply {
+            withAktoer(PersonIdent().withIdent(NorskIdent().withIdent(norskIdent)))
+            withInformasjonsbehov(informasjonsbehov)
+        }
+    }
+
+}
