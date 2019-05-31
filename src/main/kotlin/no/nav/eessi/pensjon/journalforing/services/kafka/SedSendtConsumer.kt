ccef899c76ab3f12b19c8e5e92e5c06533c825b9
Mo Amini <mo.amini@nav.no>
2019-05-31 14:35:09 +0200

    oppgaverouting

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/kafka/SedSendtConsumer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/kafka/SedSendtConsumer.kt
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
-import java.time.LocalDate
 import java.util.concurrent.CountDownLatch
 
 @Service
