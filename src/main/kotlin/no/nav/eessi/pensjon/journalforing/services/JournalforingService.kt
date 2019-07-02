da50b1bfd354889b900bf236da7de51fb9ae55e5
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2019-07-02 16:30:37 +0200

    Move HentYtelseTypeMapper.kt to its caller

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/JournalforingService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/services/JournalforingService.kt
@@ -3,7 +3,7 @@ package no.nav.eessi.pensjon.journalforing.services
 import no.nav.eessi.pensjon.journalforing.services.aktoerregister.AktoerregisterService
 import no.nav.eessi.pensjon.journalforing.services.eux.EuxService
 import no.nav.eessi.pensjon.journalforing.services.fagmodul.FagmodulService
-import no.nav.eessi.pensjon.journalforing.services.fagmodul.HentYtelseTypeMapper
+import no.nav.eessi.pensjon.journalforing.services.fagmodul.HentYtelseTypeResponse
 import no.nav.eessi.pensjon.journalforing.services.journalpost.JournalpostService
 import no.nav.eessi.pensjon.journalforing.models.sed.SedHendelseModel
 import no.nav.eessi.pensjon.journalforing.models.sed.sedMapper
@@ -113,3 +113,13 @@ class JournalforingService(val euxService: EuxService,
         return person.bostedsadresse.strukturertAdresse.landkode.value
     }
 }
+
+private class HentYtelseTypeMapper {
+    fun map(hentYtelseTypeResponse: HentYtelseTypeResponse) : OppgaveRoutingModel.YtelseType {
+        return when(hentYtelseTypeResponse.type) {
+            HentYtelseTypeResponse.YtelseType.AP -> OppgaveRoutingModel.YtelseType.AP
+            HentYtelseTypeResponse.YtelseType.GP -> OppgaveRoutingModel.YtelseType.GP
+            HentYtelseTypeResponse.YtelseType.UT -> OppgaveRoutingModel.YtelseType.UT
+        }
+    }
+ }
