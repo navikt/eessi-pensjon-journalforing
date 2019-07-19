63d14abc25909575b01ffa0e9cec388a1beae2e6
erlend-axelsson <erlend.axelsson@gmail.com>
2019-07-19 13:25:52 +0200

    Expand JournalforingService tests

--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/journalforing/JournalforingServiceTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/journalforing/JournalforingServiceTest.kt
@@ -1,5 +1,7 @@
 package no.nav.eessi.pensjon.journalforing.journalforing
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import com.nhaarman.mockito_kotlin.*
 import no.nav.eessi.pensjon.journalforing.models.BucType
 import no.nav.eessi.pensjon.journalforing.models.HendelseType
@@ -13,9 +15,9 @@ import no.nav.eessi.pensjon.journalforing.services.eux.SedDokumenterResponse
 import no.nav.eessi.pensjon.journalforing.services.fagmodul.FagmodulService
 import no.nav.eessi.pensjon.journalforing.services.fagmodul.HentYtelseTypeResponse
 import no.nav.eessi.pensjon.journalforing.services.fagmodul.Krav
-import no.nav.eessi.pensjon.journalforing.services.journalpost.JournalPostResponse
-import no.nav.eessi.pensjon.journalforing.services.journalpost.JournalpostService
+import no.nav.eessi.pensjon.journalforing.services.journalpost.*
 import no.nav.eessi.pensjon.journalforing.services.oppgave.OppgaveService
+import no.nav.eessi.pensjon.journalforing.services.oppgave.OpprettOppgaveModel
 import no.nav.eessi.pensjon.journalforing.services.personv3.PersonMock
 import no.nav.eessi.pensjon.journalforing.services.personv3.PersonV3Service
 import org.junit.Before
@@ -26,7 +28,9 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+
 
 @RunWith(MockitoJUnitRunner::class)
 class JournalforingServiceTest {
@@ -73,7 +77,7 @@ class JournalforingServiceTest {
 
         //EUX - HENT SED DOKUMENT
         doReturn(SedDokumenterResponse(
-                sed = Dokument("filnavn", MimeType.PDF, "ipsum lorem"),
+                sed = Dokument("MockSedDocument_P2000", MimeType.PDF, "ipsum lorem"),
                 vedlegg = listOf(
                         Dokument("Vedlegg1", MimeType.PDF, "ipsum lorem"),
                         Dokument("Vedlegg2", MimeType.PDF, "ipsum lorem"),
@@ -84,7 +88,7 @@ class JournalforingServiceTest {
                 .hentSedDokumenter(eq("147729"), anyString())
 
         doReturn(SedDokumenterResponse(
-                sed = Dokument("filnavn", MimeType.PDF, "ipsum lorem"),
+                sed = Dokument("MockSedDocument_P2100", MimeType.PDF, "ipsum lorem"),
                 vedlegg = listOf(
                         Dokument("Vedlegg1", null, "ipsum lorem"),
                         Dokument("Vedlegg2", null, "ipsum lorem"))))
@@ -92,7 +96,7 @@ class JournalforingServiceTest {
                 .hentSedDokumenter(eq("147730"), anyString())
 
         doReturn(SedDokumenterResponse(
-                sed = Dokument("filnavn", MimeType.PDF, "ipsum lorem"),
+                sed = Dokument("MockSedDocument_P2200", MimeType.PDF, "ipsum lorem"),
                 vedlegg = null))
                 .`when`(euxService)
                 .hentSedDokumenter(eq("148161"), anyString())
@@ -104,15 +108,19 @@ class JournalforingServiceTest {
                 .opprettJournalpost(any(), any(), eq(false))
 
         //OPPGAVEROUTING ROUTE
-        doReturn(OppgaveRoutingModel.Enhet.UFORE_UTLAND)
+        doReturn(OppgaveRoutingModel.Enhet.PENSJON_UTLAND)
                 .`when`(oppgaveRoutingService)
-                .route(any(), any(), any(), any() ,eq(null))
+                .route(any(), eq(BucType.P_BUC_01), any(), any() ,eq(null))
+
+        doReturn(OppgaveRoutingModel.Enhet.NFP_UTLAND_AALESUND)
+                .`when`(oppgaveRoutingService)
+                .route(any(), eq(BucType.P_BUC_02), any(), any() ,eq(null))
 
         doReturn(OppgaveRoutingModel.Enhet.ID_OG_FORDELING)
                 .`when`(oppgaveRoutingService)
-                .route(eq(null), any(), eq(null), any() ,eq(null))
+                .route(eq(null), eq(BucType.P_BUC_03), eq(null), any() ,eq(null))
 
-        doReturn(OppgaveRoutingModel.Enhet.NFP_UTLAND_AALESUND)
+        doReturn(OppgaveRoutingModel.Enhet.UFORE_UTLAND)
                 .`when`(oppgaveRoutingService)
                 .route(anyString(), eq(BucType.P_BUC_10), anyString(), anyString(), any())
 
@@ -128,104 +136,222 @@ class JournalforingServiceTest {
     }
 
     @Test
+    fun `gitt en sendt sed som ikke tilhører pensjon så blir den ignorert`() {
+        journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/FB_BUC_01.json"))), HendelseType.SENDT )
+        verify(oppgaveService, times(0)).opprettOppgave(any())
+        verify(journalpostService, times(0)).opprettJournalpost(any(), any(), eq(false))
+        verify(euxService, times(0)).hentSedDokumenter(anyString(), anyString())
+        verify(aktoerregisterService, times(0)).hentGjeldendeAktoerIdForNorskIdent(any())
+        verify(personV3Service, times(0)).hentPerson(any())
+        verify(fagmodulService, times(0)).hentYtelseTypeForPBuc10(any(), any())
+        verify(oppgaveRoutingService, times(0)).route(any(), any(), any(), any() ,eq(null))
+    }
+
+    @Test
     fun `Sendt gyldig Sed P2000`(){
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2000JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_01_SENDT_journalpostRequest.json")))
+        val p2000OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_01_opprettOppgaveModel.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))), HendelseType.SENDT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(personV3Service).hentPerson(eq("12378945601"))
+        verify(euxService).hentFodselsDato(eq("147729"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.SENDT) , eq(false))
+        assertEquals(p2000JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2000OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Sendt gyldig Sed P2100`(){
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2100JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_02_SENDT_journalpostRequest.json")))
+        val p2100OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_02_opprettOppgaveModel.json")))
+        val p2100BehandleSed = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_02_BehandleSed.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_02.json"))), HendelseType.SENDT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(2)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+
+        verify(personV3Service).hentPerson(eq("12378945602"))
+        verify(euxService).hentFodselsDato(eq("147730"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.SENDT) , eq(false))
+        assertEquals(p2100JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService, times(2)).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2100OppgaveModel, oppgaveCaptor.firstValue.asJson())
+        assertEquals(p2100BehandleSed, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Sendt gyldig Sed P2200`(){
+
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2200JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_03_SENDT_journalpostRequest.json")))
+        val p2200OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_03_opprettOppgaveModel.json")))
+
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_03.json"))), HendelseType.SENDT )
         verify(personV3Service, times(0)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(euxService).hentFodselsDato(eq("148161"), eq("f899bf659ff04d20bc8b978b186f1ecc"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.SENDT), eq(false))
+        assertEquals(p2200JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2200OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Sendt Sed i P_BUC_10`(){
 
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val P_BUC_10_JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_10_SENDT_journalpostRequest.json")))
+        val P_BUC_10_OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_10_opprettOppgaveModel.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_10.json"))), HendelseType.SENDT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(personV3Service).hentPerson(eq("12378945601"))
+        verify(euxService).hentFodselsDato(eq("147729"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.SENDT), eq(false))
+        assertEquals(P_BUC_10_JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(P_BUC_10_OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Sendt Sed med ugyldige verdier`(){
-        assertFailsWith<Exception> {
+        assertFailsWith<MismatchedInputException> {
             journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_01.json"))), HendelseType.SENDT )
         }
     }
 
     @Test
     fun `Sendt Sed med ugyldige felter`(){
-        assertFailsWith<Exception> {
+        assertFailsWith<MissingKotlinParameterException> {
             journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_02.json"))), HendelseType.SENDT )
         }
     }
 
     @Test
+    fun `gitt en mottatt sed som ikke tilhører pensjon så blir den ignorert`() {
+        journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/FB_BUC_01.json"))), HendelseType.SENDT )
+        verify(oppgaveService, times(0)).opprettOppgave(any())
+        verify(journalpostService, times(0)).opprettJournalpost(any(), any(), eq(false))
+        verify(euxService, times(0)).hentSedDokumenter(anyString(), anyString())
+        verify(aktoerregisterService, times(0)).hentGjeldendeAktoerIdForNorskIdent(any())
+        verify(personV3Service, times(0)).hentPerson(any())
+        verify(fagmodulService, times(0)).hentYtelseTypeForPBuc10(any(), any())
+        verify(oppgaveRoutingService, times(0)).route(any(), any(), any(), any() ,eq(null))
+    }
+
+    @Test
     fun `Mottat gyldig Sed P2000`(){
+
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2000JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_01_MOTTATT_journalpostRequest.json")))
+        val p2000OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_01_opprettOppgaveModel.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_01.json"))), HendelseType.MOTTATT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(personV3Service).hentPerson(eq("12378945601"))
+        verify(euxService).hentFodselsDato(eq("147729"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.MOTTATT) , eq(false))
+        assertEquals(p2000JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2000OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Mottat gyldig Sed P2100`(){
+
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2100JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_02_MOTTATT_journalpostRequest.json")))
+        val p2100OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_02_opprettOppgaveModel.json")))
+        val p2100BehandleSed = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_02_BehandleSed.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_02.json"))), HendelseType.MOTTATT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(2)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+
+        verify(personV3Service).hentPerson(eq("12378945602"))
+        verify(euxService).hentFodselsDato(eq("147730"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.MOTTATT) , eq(false))
+        assertEquals(p2100JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService, times(2)).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2100OppgaveModel, oppgaveCaptor.firstValue.asJson())
+        assertEquals(p2100BehandleSed, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Mottat gyldig Sed P2200`(){
+
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val p2200JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_03_MOTTATT_journalpostRequest.json")))
+        val p2200OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_03_opprettOppgaveModel.json")))
+
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_03.json"))), HendelseType.MOTTATT )
         verify(personV3Service, times(0)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(euxService).hentFodselsDato(eq("148161"), eq("f899bf659ff04d20bc8b978b186f1ecc"))
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.MOTTATT), eq(false))
+        assertEquals(p2200JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(p2200OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Mottat Sed i P_BUC_10`(){
 
+        val journalpostCaptor = argumentCaptor<JournalpostRequest>()
+        val oppgaveCaptor = argumentCaptor<OpprettOppgaveModel>()
+
+        val P_BUC_10_JournalpostRequest = String(Files.readAllBytes(Paths.get("src/test/resources/journalpost/P_BUC_10_MOTTATT_journalpostRequest.json")))
+        val P_BUC_10_OppgaveModel = String(Files.readAllBytes(Paths.get("src/test/resources/oppgave/P_BUC_10_opprettOppgaveModel.json")))
+
         journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/P_BUC_10.json"))), HendelseType.MOTTATT )
-        verify(personV3Service, times(1)).hentPerson(any())
-        verify(euxService, times(1)).hentFodselsDato(any(), any())
-        verify(oppgaveService, times(1)).opprettOppgave(any())
-        verify(journalpostService, times(1)).opprettJournalpost(any(), any(), eq(false))
+        verify(personV3Service).hentPerson(eq("12378945601"))
+        verify(euxService).hentFodselsDato(eq("147729"), eq("b12e06dda2c7474b9998c7139c841646"))
+
+
+        verify(journalpostService).opprettJournalpost(journalpostCaptor.capture(), eq(HendelseType.MOTTATT), eq(false))
+        assertEquals(P_BUC_10_JournalpostRequest, journalpostCaptor.lastValue.toString())
+
+        verify(oppgaveService).opprettOppgave(oppgaveCaptor.capture())
+        assertEquals(P_BUC_10_OppgaveModel, oppgaveCaptor.lastValue.asJson())
     }
 
     @Test
     fun `Mottat Sed med ugyldige verdier`(){
-        assertFailsWith<Exception> {
-            journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_01.json"))), HendelseType.MOTTATT )
+        assertFailsWith<MismatchedInputException> {
+                journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_01.json"))), HendelseType.MOTTATT )
         }
     }
 
     @Test
     fun `Mottat Sed med ugyldige felter`(){
-        assertFailsWith<Exception> {
-            journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_02.json"))), HendelseType.MOTTATT )
+        assertFailsWith<MissingKotlinParameterException> {
+                journalforingService.journalfor(String(Files.readAllBytes(Paths.get("src/test/resources/sed/BAD_BUC_02.json"))), HendelseType.MOTTATT )
         }
     }
 
--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/services/personv3/PersonMock.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/services/personv3/PersonMock.kt
@@ -3,8 +3,12 @@ package no.nav.eessi.pensjon.journalforing.services.personv3
 import no.nav.tjeneste.virksomhet.person.v3.informasjon.*
 
 object PersonMock {
-    internal fun createWith(landkoder: Boolean = true): Person? =
-            Person().withPersonnavn(Personnavn().withEtternavn("Testesen").withFornavn("Test"))
+    internal fun createWith(landkoder: Boolean = true, fornavn: String = "Test", etternavn: String = "Testesen"):
+            Person? = Person()
+                    .withPersonnavn(Personnavn()
+                            .withEtternavn(etternavn)
+                            .withFornavn(fornavn)
+                            .withSammensattNavn("$fornavn $etternavn"))
                     .withKjoenn(Kjoenn().withKjoenn(Kjoennstyper().withValue("M")))
                     .withStatsborgerskap(Statsborgerskap().withLand(Landkoder().withValue("NOR")))
                     .withBostedsadresse(Bostedsadresse()
