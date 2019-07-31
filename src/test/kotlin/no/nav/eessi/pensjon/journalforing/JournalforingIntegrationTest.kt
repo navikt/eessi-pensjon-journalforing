7a3c60ef13acdef097624b99e51446bc9c2957d7
Mo Amini <mo.amini@nav.no>
2019-07-31 07:53:49 +0200

    Revert "Et forsøk på å fikse intermittent testfeil (#60)"
    This reverts commit 3f3f3495

--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingIntegrationTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingIntegrationTest.kt
@@ -34,7 +34,6 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.LocalDate
 import java.util.*
-import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import javax.ws.rs.HttpMethod
 
@@ -54,9 +53,6 @@ class JournalforingIntegrationTest {
     @Autowired
     lateinit var  personV3Service: PersonV3Service
 
-    private val latch = CountDownLatch(4)
-
-
     @Test
     fun `Når en sedSendt hendelse blir konsumert skal det opprettes journalføringsoppgave for pensjon SEDer`() {
 
@@ -74,8 +70,7 @@ class JournalforingIntegrationTest {
         // produserer sedSendt meldinger på kafka
         produserSedHendelser(sedSendtProducerTemplate)
 
-        // Venter på at begge konsumentgruppene (sedListener og testUtilityConsumer) skal consumeSedSendt meldingene
-        latch.await(15000, TimeUnit.MILLISECONDS)
+        // Venter på at sedListener skal consumeSedSendt meldingene
         sedListener.getLatch().await(15000, TimeUnit.MILLISECONDS)
 
         // Verifiserer alle kall
@@ -121,9 +116,7 @@ class JournalforingIntegrationTest {
         val consumerFactory = DefaultKafkaConsumerFactory<String, String>(consumerProperties)
         val containerProperties = ContainerProperties(topicNavn)
         val container = KafkaMessageListenerContainer<String, String>(consumerFactory, containerProperties)
-        val messageListener = MessageListener<String, String> { record -> println("Konsumerer melding:  $record")
-            latch.countDown()
-        }
+        val messageListener = MessageListener<String, String> { record -> println("Konsumerer melding:  $record") }
         container.setupMessageListener(messageListener)
 
         return container
@@ -504,14 +497,6 @@ class JournalforingIntegrationTest {
                                 listOf(
                                         Parameter("identgruppe", "AktoerId"),
                                         Parameter("gjeldende", "true"))),
-                VerificationTimes.exactly(1)
-        )
-
-        // Verifiserer at det har blitt forsøkt å hente well known STS configurasjon
-        mockServer.verify(
-                request()
-                        .withMethod(HttpMethod.GET)
-                        .withPath("/.well-known/openid-configuration"),
                 VerificationTimes.exactly(1)
         )
 
