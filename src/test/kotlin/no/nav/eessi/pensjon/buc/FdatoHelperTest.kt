16a3239f8e6702c59085685b792941a9bf7da1e9
Mo Amini <mo.amini@nav.no>
2019-12-23 14:36:54 +0100

    Stor refactoring
    FdatoService er ikke lenger en service , det er en helper uten avhengigheter til noen servicer
    FnrService er ikke lenger en service, det er en helper uten avhengigheter til noen servicer
    Identifisering av person skjer nå et sted og populerer JournalforingPerson som brukes videre isteder for mange enkeltstående parametere

---
+++ src/test/kotlin/no/nav/eessi/pensjon/buc/FdatoHelperTest.kt
@@ -0,0 +1,48 @@
+package no.nav.eessi.pensjon.buc
+
+import no.nav.eessi.pensjon.json.validateJson
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class FdatoHelperTest {
+
+    private lateinit var helper: FdatoHelper
+
+    @BeforeEach
+    fun setup() {
+        helper = FdatoHelper()
+    }
+
+    @Test
+    fun `Calling getFDatoFromSed returns exception when foedselsdato is not found` () {
+        org.junit.jupiter.api.assertThrows<RuntimeException> {
+            helper.finnFDatoFraSeder(listOf(getTestJsonFile("EmptySED.json")))
+        }
+    }
+
+    @Test
+    fun `Calling getFDatoFromSed returns valid fdato when found in first valid SED` () {
+        val actual = helper.finnFDatoFraSeder(listOf(
+                getTestJsonFile("P2100-PinDK-NAV.json"),
+                getTestJsonFile("P2000-NAV.json"),
+                getTestJsonFile("P15000-NAV.json")))
+        val expected = "1969-09-11"
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Calling getFDatoFromSed   returns valid resultset on BUC_01` () {
+        assertEquals("1980-01-01", helper.finnFDatoFraSeder(listOf(getTestJsonFile("P2000-NAV.json"))))
+    }
+
+    private fun getTestJsonFile(filename: String): String {
+        val filepath = "src/test/resources/buc/${filename}"
+        val json = String(Files.readAllBytes(Paths.get(filepath)))
+        assertTrue(validateJson(json))
+        return json
+    }
+}
