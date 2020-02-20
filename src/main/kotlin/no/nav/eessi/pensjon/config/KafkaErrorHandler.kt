6cbbfdb2557a37cea6522de64e84bc1514552d5f
moamini <mo.amini@nav.no>
2020-02-20 13:30:24 +0100

    Logges exception og meldingsinnhold ved kafka konsumering

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaErrorHandler.kt
@@ -4,9 +4,13 @@ import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
-import org.springframework.kafka.listener.*
+import org.springframework.kafka.listener.ContainerAwareErrorHandler
+import org.springframework.kafka.listener.ContainerStoppingErrorHandler
+import org.springframework.kafka.listener.MessageListenerContainer
 import org.springframework.stereotype.Component
-import java.lang.Exception
+import java.io.PrintWriter
+import java.io.StringWriter
+
 
 @Profile("prod")
 @Component
@@ -15,8 +19,24 @@ class KafkaErrorHandler : ContainerAwareErrorHandler {
 
     private val stopper = ContainerStoppingErrorHandler()
 
-    override fun handle(thrownException: Exception?, records: MutableList<ConsumerRecord<*, *>>?, consumer: Consumer<*, *>?, container: MessageListenerContainer?) {
-        logger.error("En feil oppstod under kafka konsumering. Stopper containeren ! Restart er nødvendig for å fortsette konsumering")
+    override fun handle(thrownException: Exception?,
+                        records: MutableList<ConsumerRecord<*, *>>?,
+                        consumer: Consumer<*, *>?, container: MessageListenerContainer?) {
+        val stacktrace = StringWriter()
+        thrownException?.printStackTrace(PrintWriter(stacktrace))
+
+        logger.error("En feil oppstod under kafka konsumering av meldinger: \n ${hentMeldinger(records)} \n" +
+                "Stopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
         stopper.handle(thrownException, records, consumer, container)
+    }
+
+    fun hentMeldinger(records: MutableList<ConsumerRecord<*, *>>?): String {
+        var meldinger = ""
+        records?.forEach { it ->
+            meldinger += "--------------------------------------------------------------------------------\n"
+            meldinger += it.toString()
+            meldinger += "\n"
+        }
+        return meldinger
     }
  }
