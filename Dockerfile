e66303452f696a45e686f632bc6bf92c11fcf290
moamini <mo.amini@nav.no>
2020-02-28 13:55:57 +0100

    Fjernes unødvendig config Legges appdynamics config til i prod

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,5 @@ COPY nais/export-vault-secrets.sh /init-scripts/
 
 RUN chmod +x /init-scripts/*
 
-ENV APPD_NAME eessi-pensjon
-ENV APPD_TIER journalforing
-ENV APPD_ENABLED true
-#ENV APPDYNAMICS_CONTROLLER_HOST_NAME appdynamics.adeo.no
-#ENV APPDYNAMICS_AGENT_ACCOUNT_NAME NON-PROD
-#ENV APPDYNAMICS_CONTROLLER_PORT 443
-#ENV APPDYNAMICS_CONTROLLER_SSL_ENABLED true
+ENV APPD_NAME eessi-pensjon-journalforing
+ENV APPD_TIER eessipensjon
--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -13,10 +13,3 @@ then
     echo "Setter eessi-pensjon-journalforing srvUsername"
     export srvusername=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/username)
 fi
-
-#echo "Sjekker APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"
-#if test -f /var/run/secrets/nais.io/appdynamics/APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY;
-#then
-#    echo "Setter APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"
-#    export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(cat /var/run/secrets/nais.io/appdynamics/APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)
-#fi
