8a9fdea788e300fbc5ed2920c9b5d5ac4024b94b
mariampervez <mariam.pervez@nav.no>
2020-04-27 14:41:42 +0200

    mellomlagrer i forbindelse med rebase

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -49,7 +49,7 @@ class SedListener(
                         val sedHendelse = SedHendelseModel.fromJson(hendelse)
                         logger.info("*** Starter utgående journalføring for SED ${sedHendelse.sedType} BUCtype: ${sedHendelse.bucType} bucid: ${sedHendelse.rinaSakId} ***")
                         val alleSedIBuc  = sedDokumentHelper.hentAlleSedIBuc(sedHendelse.rinaSakId)
-                        val identifisertPerson = personidentifiseringService.identifiserPerson(sedHendelse, sedDokumentHelper.hentAlleSeds(alleSedIBuc))
+                        val identifisertPerson = personidentifiseringService.identifiserPerson(sedHendelse.navBruker, sedDokumentHelper.hentAlleSeds(alleSedIBuc))
                         val ytelseType = sedDokumentHelper.hentYtelseType(sedHendelse,alleSedIBuc)
                         journalforingService.journalfor(sedHendelse, SENDT, identifisertPerson, ytelseType, offset)
                     }
@@ -83,7 +83,7 @@ class SedListener(
                             val sedHendelse = SedHendelseModel.fromJson(hendelse)
                             logger.info("*** Starter innkommende journalføring for SED ${sedHendelse.sedType} BUCtype: ${sedHendelse.bucType} bucid: ${sedHendelse.rinaSakId} ***")
                             val alleSedIBuc = sedDokumentHelper.hentAlleSedIBuc(sedHendelse.rinaSakId)
-                            val identifisertPerson = personidentifiseringService.identifiserPerson(sedHendelse, sedDokumentHelper.hentAlleSeds(alleSedIBuc))
+                            val identifisertPerson = personidentifiseringService.identifiserPerson(sedHendelse.navBruker, sedDokumentHelper.hentAlleSeds(alleSedIBuc))
                             val ytelseType = sedDokumentHelper.hentYtelseType(sedHendelse,alleSedIBuc)
                             journalforingService.journalfor(sedHendelse, MOTTATT, identifisertPerson, ytelseType, offset)
                     }
