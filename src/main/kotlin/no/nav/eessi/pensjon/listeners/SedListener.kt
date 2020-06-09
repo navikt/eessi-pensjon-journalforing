307f8294b3edbecbe29cb62b12b196865d434b13
moamini <mo.amini@nav.no>
2020-06-09 13:04:41 +0200

    Kommenterer ut recovery listener

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.annotation.PartitionOffset
-import org.springframework.kafka.annotation.TopicPartition
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 import java.util.*
@@ -117,15 +115,15 @@ class SedListener(
         }
     }
 
-    @KafkaListener(groupId = "\${kafka.sedSendt.groupid}-recovery",
-            topicPartitions = [TopicPartition(topic = "\${kafka.sedSendt.topic}",
-                    partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "25196")])])
-    fun recoverConsumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        if(cr.offset() == 25196L) {
-            logger.info("Behandler sedSendt offset: ${cr.offset()}")
-            consumeSedSendt(hendelse, cr, acknowledgment)
-        } else {
-            acknowledgment.acknowledge()
-        }
-    }
+//    @KafkaListener(groupId = "\${kafka.sedSendt.groupid}-recovery",
+//            topicPartitions = [TopicPartition(topic = "\${kafka.sedSendt.topic}",
+//                    partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "25196")])])
+//    fun recoverConsumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+//        if(cr.offset() == 25196L) {
+//            logger.info("Behandler sedSendt offset: ${cr.offset()}")
+//            consumeSedSendt(hendelse, cr, acknowledgment)
+//        } else {
+//            acknowledgment.acknowledge()
+//        }
+//    }
 }
