f022b02f28c9785bfefe6f46644ebed268fde97c
Daniel Skarpås <dskarpas@gmail.com>
2020-11-17 14:45:25 +0100

    legger inn P9000 i gylide sed liste
    Co-authored-by: Jens Christian Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/buc/SedDokumentHelper.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/buc/SedDokumentHelper.kt
@@ -84,7 +84,7 @@ class SedDokumentHelper(private val fagmodulKlient: FagmodulKlient,
 
     private fun filterUtGyldigSedId(alleDokumenterJsonNode: JsonNode): List<Pair<String, String>> {
         val validSedtype = listOf("P2000","P2100","P2200","P1000",
-                "P5000","P6000","P7000", "P8000",
+                "P5000","P6000","P7000", "P8000", "P9000",
                 "P10000","P1100","P11000","P12000","P14000","P15000", "H070", "R005")
 
         return alleDokumenterJsonNode
