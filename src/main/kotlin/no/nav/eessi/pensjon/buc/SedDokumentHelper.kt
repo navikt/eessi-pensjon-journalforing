7968bbea9dd869551a2acf1a85bfd0c441df48bc
moamini <mo.amini@nav.no>
2020-11-19 12:47:43 +0100

    Revert "Fjerner filtering av sed"
    This reverts commit c4d28f59

--- src/main/kotlin/no/nav/eessi/pensjon/buc/SedDokumentHelper.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/buc/SedDokumentHelper.kt
@@ -84,10 +84,15 @@ class SedDokumentHelper(private val fagmodulKlient: FagmodulKlient,
 
     private fun filterUtGyldigSedId(alleDokumenter: String?): List<SediBuc> {
         val alleDokumenterJsonNode = mapper.readTree(alleDokumenter)
+        val validSedtype = listOf("P2000","P2100","P2200","P1000",
+                "P5000","P6000","P7000", "P8000", "P9000",
+                "P10000","P1100","P11000","P12000","P14000","P15000", "H070", "R005")
 
         return alleDokumenterJsonNode
                 .asSequence()
                 .filterNot { rootNode -> rootNode.get("status").textValue() =="empty" }
+//                .filter { rootNode-> rootNode.get("status").textValue() == "received" || rootNode.get("status").textValue() == "sent" }
+                .filter { rootNode ->  validSedtype.contains(rootNode.get("type").textValue()) }
                 .map { validSeds -> SediBuc(id = validSeds.get("id").textValue(),type = SedType.valueOf(validSeds.get("type").textValue()), status = validSeds.get("status").textValue()) }
                 .sortedBy { it.type }
                 .toList()
