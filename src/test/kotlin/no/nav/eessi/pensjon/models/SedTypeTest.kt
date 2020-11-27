cef73fe57c5e4ab943e330b63f36807369fdd830
NN <nicolas.nordhagen@nav.no>
2020-11-27 12:49:19 +0100

    Oppdatering av serde-tester

--- src/test/kotlin/no/nav/eessi/pensjon/models/SedTypeTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/models/SedTypeTest.kt
@@ -1,5 +1,22 @@
 package no.nav.eessi.pensjon.models
 
-internal class SedTypeTest {
+import no.nav.eessi.pensjon.json.mapJsonToAny
+import no.nav.eessi.pensjon.json.toJson
+import no.nav.eessi.pensjon.json.typeRefs
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
+internal class SedTypeTest {
+    @ParameterizedTest
+    @EnumSource(SedType::class)
+    fun `Verifiser serde av SedType fungerer`(type: SedType) {
+        Assertions.assertEquals(type, serde(type))
+    }
+
+    private fun serde(sedType: SedType): SedType {
+        val json = sedType.toJson()
+
+        return mapJsonToAny(json, typeRefs())
+    }
 }
