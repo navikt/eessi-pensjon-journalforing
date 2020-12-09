15c0d018b285be7ed57f701903a2b52d31a5aa56
NN <nicolas.nordhagen@nav.no>
2020-12-09 10:35:02 +0100

    Tester for P_BUC_01 og 04

--- src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/Pbuc04.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/Pbuc04.kt
@@ -2,11 +2,14 @@ package no.nav.eessi.pensjon.oppgaverouting
 
 import no.nav.eessi.pensjon.models.Enhet
 
+/**
+ * P_BUC_04: Anmodning  om opplysninger om perioder med omsorg for barn
+ */
 class Pbuc04 : BucTilEnhetHandler {
     override fun hentEnhet(request: OppgaveRoutingRequest): Enhet {
         return when {
             erStrengtFortrolig(request.diskresjonskode) -> Enhet.DISKRESJONSKODE
-            kanAutomatiskJournalfores(request)-> Enhet.AUTOMATISK_JOURNALFORING
+            kanAutomatiskJournalfores(request) -> Enhet.AUTOMATISK_JOURNALFORING
             request.bosatt == Bosatt.NORGE -> Enhet.NFP_UTLAND_AALESUND
             else -> Enhet.PENSJON_UTLAND
         }
