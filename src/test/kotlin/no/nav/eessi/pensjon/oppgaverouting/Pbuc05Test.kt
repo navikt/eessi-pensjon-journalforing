1345c81ce06511188740f98487351a2fc38eae53
MariamPervez <35491763+MariamPervez@users.noreply.github.com>
2021-04-09 14:31:14 +0200

    Feature/alder18til62 (#232)
    * EP-1127: Endrer aldersjekk for alle land på bruker mellom 18-62år
    * EP-1130: Endrer aldersjekk mellom 18-60 for P_BUC_05 når avsenderland er Tyskland
    
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

---
+++ src/test/kotlin/no/nav/eessi/pensjon/oppgaverouting/Pbuc05Test.kt
@@ -0,0 +1,92 @@
+package no.nav.eessi.pensjon.oppgaverouting
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.eessi.pensjon.models.BucType
+import no.nav.eessi.pensjon.models.Enhet
+import no.nav.eessi.pensjon.personidentifisering.helpers.Fodselsnummer
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Pbuc05Test {
+
+    private val handler = BucTilEnhetHandlerCreator.getHandler(BucType.P_BUC_05) as Pbuc05
+
+    companion object {
+        private const val FNR_63 = "05115743432"        // SLAPP DORULL 62
+        private const val FNR_61 = "12105928066"        // STERK UTEDO
+        private const val FNR_54 = "11067122781"    // KRAFTIG VEGGPRYD
+        private const val FNR_BARN = "12011577847"      // STERK BUSK
+    }
+
+    @Test
+    fun `Bosatt norge`() {
+        val request = mockk<OppgaveRoutingRequest>(relaxed = true) {
+            every { bosatt } returns Bosatt.NORGE
+        }
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_61)!!.getBirthDate()
+        assertEquals(Enhet.UFORE_UTLANDSTILSNITT, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_63)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_BARN)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+    }
+
+    @Test
+    fun `Bosatt norge avsenderland Tyskland uføre mellom 18 til 60 år`() {
+        val request = mockk<OppgaveRoutingRequest>(relaxed = true) {
+            every { bosatt } returns Bosatt.NORGE
+            every { avsenderLand } returns "DE"
+        }
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_61)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_54)!!.getBirthDate()
+        assertEquals(Enhet.UFORE_UTLANDSTILSNITT, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_BARN)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+
+    }
+
+
+    @Test
+    fun `Bosatt utland avsenderland Tyskland uføre mellom 18 til 60 år`() {
+        val request = mockk<OppgaveRoutingRequest>(relaxed = true) {
+            every { bosatt } returns Bosatt.UTLAND
+            every { avsenderLand } returns "DE"
+        }
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_63)!!.getBirthDate()
+        assertEquals(Enhet.PENSJON_UTLAND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_54)!!.getBirthDate()
+        assertEquals(Enhet.UFORE_UTLAND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_BARN)!!.getBirthDate()
+        assertEquals(Enhet.PENSJON_UTLAND, handler.hentEnhet(request))
+
+    }
+
+    @Test
+    fun `Bosatt norge avsenderland Danmark uføre mellom 18 til 62 år`() {
+        val request = mockk<OppgaveRoutingRequest>(relaxed = true) {
+            every { bosatt } returns Bosatt.NORGE
+            every { avsenderLand } returns "DK"
+        }
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_63)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_BARN)!!.getBirthDate()
+        assertEquals(Enhet.NFP_UTLAND_AALESUND, handler.hentEnhet(request))
+
+        every { request.fdato } returns Fodselsnummer.fra(FNR_54)!!.getBirthDate()
+        assertEquals(Enhet.UFORE_UTLANDSTILSNITT, handler.hentEnhet(request))
+    }
+
+}
