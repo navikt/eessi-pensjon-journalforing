bbd9d0f328968df34e2cd099d116fe58c41c74cf
moamini <mo.amini@nav.no>
2021-05-11 08:08:34 +0200

    Midlertidig full interceptor logging

--- src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
@@ -6,18 +6,15 @@ import no.nav.eessi.pensjon.logging.RequestResponseLoggerInterceptor
 import no.nav.eessi.pensjon.metrics.RequestCountInterceptor
 import no.nav.eessi.pensjon.security.sts.STSService
 import no.nav.eessi.pensjon.security.sts.UsernameToOidcInterceptor
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpRequest
 import org.springframework.http.MediaType
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.http.client.ClientHttpResponse
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
-import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.client.*
 import org.springframework.http.client.support.BasicAuthenticationInterceptor
 import org.springframework.web.client.DefaultResponseErrorHandler
 import org.springframework.web.client.RestTemplate
@@ -60,7 +57,7 @@ class RestTemplateConfig(private val securityTokenExchangeService: STSService, p
                 .rootUri(joarkUrl)
                 .errorHandler(DefaultResponseErrorHandler())
                 .additionalInterceptors(
-                        RequestResponseLoggerInterceptor(),
+                    FullRequestResponseLoggerInterceptor(),
                         RequestIdHeaderInterceptor(),
                         RequestCountInterceptor(meterRegistry),
                         UsernameToOidcInterceptor(securityTokenExchangeService))
@@ -155,3 +152,46 @@ class RestTemplateConfig(private val securityTokenExchangeService: STSService, p
         }
     }
 }
+
+
+
+class FullRequestResponseLoggerInterceptor : ClientHttpRequestInterceptor {
+    private val log: Logger by lazy { LoggerFactory.getLogger(RequestResponseLoggerInterceptor::class.java) }
+
+    override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
+
+        logRequest(request, body)
+        val response: ClientHttpResponse = execution.execute(request, body)
+        logResponse(response)
+        return response
+    }
+
+    private fun logRequest(request: HttpRequest, body: ByteArray) {
+        if (log.isDebugEnabled) {
+            val requestLog = StringBuffer()
+
+            requestLog.append("\n===========================request begin================================================")
+            requestLog.append("\nURI            :  ${request.uri}")
+            requestLog.append("\nMethod         :  ${request.method}")
+            requestLog.append("\nHeaders        :  ${request.headers}")
+            requestLog.append(body)
+            requestLog.append("\n==========================request end================================================")
+            log.debug(requestLog.toString())
+        }
+    }
+
+    private fun logResponse(response: ClientHttpResponse) {
+        if (log.isDebugEnabled) {
+            val responseLog = StringBuilder()
+
+            responseLog.append("\n===========================response begin================================================")
+            responseLog.append("\nStatus code    : ${response.statusCode}")
+            responseLog.append("\nStatus text    : ${response.statusText}")
+            responseLog.append("\nHeaders        : ${response.headers}")
+            responseLog.append(response.body.readBytes())
+            responseLog.append("\n==========================response end================================================")
+            log.debug(responseLog.toString())
+        }
+    }
+}
+
