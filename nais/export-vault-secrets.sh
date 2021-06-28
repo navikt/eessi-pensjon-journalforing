c4494b6cbe8d33542858af2e665368dd07155ef7
moamini <mo.amini@nav.no>
2021-06-28 11:18:26 +0200

    setter opp team namespace

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -13,3 +13,20 @@ then
     echo "Setter eessi-pensjon-journalforing srvUsername"
     export srvusername=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing/username)
 fi
+
+
+# Team namespace
+
+echo "Sjekker srvpassword eessi-pensjon-journalforing q2 i team namespace"
+if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing-q2/password;
+then
+  echo "Setter srvpassword eessi-pensjon-journalforing q2 i team namespace"
+    export srvpassword=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing-q2/password)
+fi
+
+echo "Sjekker srvusername i eessi-pensjon-journalforing q2 i team namespace"
+if test -f /var/run/secrets/nais.io/srveessi-pensjon-journalforing-q2/username;
+then
+    echo "Setter srvusername i eessi-pensjon-journalforing q2 i team namespace"
+    export srvusername=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing-q2/username)
+fi
