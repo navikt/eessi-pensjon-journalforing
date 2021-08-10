4a1d8074801751bafd852dfca2e9a9a53713c9bc
mariampervez <mariam.pervez@nav.no>
2021-08-10 13:43:44 +0200

    Revert av melding til reprosessering

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -21,8 +21,6 @@ import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.annotation.PartitionOffset
-import org.springframework.kafka.annotation.TopicPartition
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 import java.util.*
@@ -241,15 +239,15 @@ class SedListener(
      * Ikke slett funksjonene under før vi har et bedre opplegg for tilbakestilling av topic.
      * Se jira-sak: EP-968
      **/
-    @KafkaListener(groupId = "\${kafka.sedSendt.groupid}-recovery",
-            topicPartitions = [TopicPartition(topic = "\${kafka.sedSendt.topic}",
-                    partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "124390")])])
-    fun recoverConsumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        if (cr.offset() == 124390L) {
-            logger.info("Behandler sedSendt offset: ${cr.offset()}")
-            consumeSedSendt(hendelse, cr, acknowledgment)
-        }
-    }
+//    @KafkaListener(groupId = "\${kafka.sedSendt.groupid}-recovery",
+//            topicPartitions = [TopicPartition(topic = "\${kafka.sedSendt.topic}",
+//                    partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "124390")])])
+//    fun recoverConsumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+//        if (cr.offset() == 124390L) {
+//            logger.info("Behandler sedSendt offset: ${cr.offset()}")
+//            consumeSedSendt(hendelse, cr, acknowledgment)
+//        }
+//    }
 
      /*
 
