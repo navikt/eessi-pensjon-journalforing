1a1719367279223e8440901bee0cdb4823126460
mariampervez <mariam.pervez@nav.no>
2021-08-23 14:44:37 +0200

    Rekjører melding med offset 129004 utgående P_BUC_01
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -68,7 +68,7 @@ class SedListener(
                     throw RuntimeException("Applikasjonen har forsøkt å prosessere sedSendt meldinger fra offset 0, stopper prosessering")
                 }
                 logger.debug(hendelse)
-                val offsetToSkip = listOf(118452L, 129004L)
+                val offsetToSkip = listOf(118452L)
                 try {
                     val offset = cr.offset()
                     if (offsetToSkip.contains(offset)) {
