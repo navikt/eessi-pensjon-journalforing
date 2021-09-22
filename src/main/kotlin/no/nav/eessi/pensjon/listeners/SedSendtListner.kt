b1090d5d60bdfa29e7b89cf8c8c179bc602ef7a4
mariampervez <mariam.pervez@nav.no>
2021-09-22 11:08:16 +0200

    Hopper over offset 139287

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListner.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListner.kt
@@ -65,7 +65,7 @@ class SedSendtListner(
                     throw RuntimeException("Applikasjonen har forsøkt å prosessere sedSendt meldinger fra offset 0, stopper prosessering")
                 }
                 logger.debug(hendelse)
-                val offsetToSkip = listOf(118452L, 8507214L)
+                val offsetToSkip = listOf(118452L, 139287L)
                 try {
                     val offset = cr.offset()
                     if (offsetToSkip.contains(offset)) {
