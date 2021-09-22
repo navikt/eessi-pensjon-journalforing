e0296c982664e745db9bf7fce325fb9c62434ee2
mariampervez <mariam.pervez@nav.no>
2021-09-22 14:54:16 +0200

    Skipper offset 139333

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListner.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListner.kt
@@ -65,7 +65,7 @@ class SedSendtListner(
                     throw RuntimeException("Applikasjonen har forsøkt å prosessere sedSendt meldinger fra offset 0, stopper prosessering")
                 }
                 logger.debug(hendelse)
-                val offsetToSkip = listOf(118452L, 139287L)
+                val offsetToSkip = listOf(118452L, 139287L, 139333L)
                 try {
                     val offset = cr.offset()
                     if (offsetToSkip.contains(offset)) {
