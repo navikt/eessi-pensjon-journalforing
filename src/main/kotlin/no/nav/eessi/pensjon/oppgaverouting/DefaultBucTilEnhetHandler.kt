f3546e5bb8e6b78fb791957d37b5f29a6495f433
moamini <mo.amini@nav.no>
2021-10-04 08:33:02 +0200

    Legger til mer logging for routingregler

--- src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/DefaultBucTilEnhetHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/DefaultBucTilEnhetHandler.kt
@@ -4,8 +4,10 @@ import no.nav.eessi.pensjon.models.Enhet
 
 class DefaultBucTilEnhetHandler : BucTilEnhetHandler {
     override fun hentEnhet(request: OppgaveRoutingRequest): Enhet {
-        return if (request.harAdressebeskyttelse)
+        return if (request.harAdressebeskyttelse) {
+            adresseBeskyttelseLogging(request.sedType, request.bucType, Enhet.DISKRESJONSKODE)
             Enhet.DISKRESJONSKODE
+        }
         else
             enhetFraAlderOgLand(request)
     }
@@ -14,11 +16,23 @@ class DefaultBucTilEnhetHandler : BucTilEnhetHandler {
         val ageIsBetween18and62 = request.fdato.ageIsBetween18and62()
 
         return if (request.bosatt == Bosatt.NORGE) {
-            if (ageIsBetween18and62) Enhet.UFORE_UTLANDSTILSNITT
-            else Enhet.NFP_UTLAND_AALESUND
+            if (ageIsBetween18and62) {
+                logger.info("Router ${request.sedType} i ${request.bucType} til ${Enhet.UFORE_UTLANDSTILSNITT} på grunn av personen er bosatt i norge og alder er NAY")
+                Enhet.UFORE_UTLANDSTILSNITT
+            }
+            else  {
+                logger.info("Router ${request.sedType} i ${request.bucType} til ${Enhet.NFP_UTLAND_AALESUND} på grunn av personen er bosatt i utlandet og alder er NFP")
+                Enhet.NFP_UTLAND_AALESUND
+            }
         } else {
-            if (ageIsBetween18and62) Enhet.UFORE_UTLAND
-            else Enhet.PENSJON_UTLAND
+            if (ageIsBetween18and62) {
+                logger.info("Router ${request.sedType} i ${request.bucType} til ${Enhet.UFORE_UTLAND} på grunn av personen er bosatt i utlandet og alder er NAY")
+                Enhet.UFORE_UTLAND
+            }
+            else {
+                logger.info("Router ${request.sedType} i ${request.bucType} til ${Enhet.PENSJON_UTLAND} på grunn av personen er bosatt i utlandet og alder er NFP")
+                Enhet.PENSJON_UTLAND
+            }
         }
     }
 }
