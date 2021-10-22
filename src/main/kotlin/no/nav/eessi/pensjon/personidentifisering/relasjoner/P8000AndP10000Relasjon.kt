d4a9f6b3142c22a01b398e2856760002e8665b59
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-10-22 14:46:18 +0200

    ep-1226 Fiksa biffen...
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/relasjoner/P8000AndP10000Relasjon.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/relasjoner/P8000AndP10000Relasjon.kt
@@ -16,36 +16,44 @@ class P8000AndP10000Relasjon(private val sed: SED, private val bucType: BucType,
         val fnrListe = mutableListOf<SEDPersonRelasjon>()
         logger.info("Leter etter gyldig ident og relasjon(er) i SedType: ${sed.type}")
 
+
         val forsikret = hentForsikretPerson(bestemSaktype(bucType))
 
-        //Annenperson søker/barn o.l
+        hentAnnenpersonRelasjon()?.let { fnrListe.add(it) }
+
+        logger.debug("forsikret $forsikret")
+        logger.debug("gjenlevlist: $fnrListe")
+
+        if (fnrListe.firstOrNull { it.relasjon == Relasjon.BARN || it.relasjon == Relasjon.FORSORGER } != null ) {
+            return fnrListe + forsikret
+        }
+        return fnrListe.ifEmpty { forsikret }
+    }
+
+    //Annenperson søker/barn o.l
+    fun hentAnnenpersonRelasjon(): SEDPersonRelasjon? {
         if (bucType == BucType.P_BUC_05 || bucType == BucType.P_BUC_10 || bucType == BucType.P_BUC_02) {
             val annenPerson = sed.nav?.annenperson?.person
 
+            logger.debug("annenPerson: $annenPerson")
             annenPerson?.let { person ->
-                    val sokAnnenPersonKriterie =  opprettSokKriterie(person)
-                    val annenPersonPin = Fodselsnummer.fra(person.pin?.firstOrNull { it.land == "NO" }?.identifikator)
-                    val annenPersonFdato = mapFdatoTilLocalDate(person.foedselsdato)
-                    val rolle = person.rolle
-                    val annenPersonRelasjon = when (rolle) {
-                            //Rolle barn benyttes ikke i noe journalføring hendelse kun hente ut for...?
-                            BARN.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.BARN, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
-                            //Rolle forsorger benyttes ikke i noe journalføring hendelse...
-                            FORSORGER.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.FORSORGER, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
-                            //etterlatte benyttes i journalføring hendelse..
-                            ETTERLATTE.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.GJENLEVENDE, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
-                            else -> null
-                        }
-                        annenPersonRelasjon?.let {
-                            fnrListe.add(it)
-                        }
+                val sokAnnenPersonKriterie =  opprettSokKriterie(person)
+                val annenPersonPin = Fodselsnummer.fra(person.pin?.firstOrNull { it.land == "NO" }?.identifikator)
+                val annenPersonFdato = mapFdatoTilLocalDate(person.foedselsdato)
+                val rolle = person.rolle
+                val annenPersonRelasjon = when (rolle) {
+                    //Rolle barn benyttes ikke i noe journalføring hendelse kun hente ut for...?
+                    BARN.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.BARN, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
+                    //Rolle forsorger benyttes ikke i noe journalføring hendelse...
+                    FORSORGER.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.FORSORGER, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
+                    //etterlatte benyttes i journalføring hendelse..
+                    ETTERLATTE.kode -> SEDPersonRelasjon(annenPersonPin, Relasjon.GJENLEVENDE, sedType = sed.type, sokKriterier = sokAnnenPersonKriterie , fdato = annenPersonFdato, rinaDocumentId = rinaDocumentId)
+                    else -> null
                 }
+                return annenPersonRelasjon
+            }
         }
-        if (fnrListe.firstOrNull { it.relasjon == Relasjon.BARN || it.relasjon == Relasjon.FORSORGER} != null) {
-            fnrListe.addAll(forsikret)
-            return fnrListe
-        }
-        return fnrListe.ifEmpty { forsikret }
+        return null
     }
 
 }
