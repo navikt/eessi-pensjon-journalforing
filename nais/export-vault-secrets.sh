5be7ed106534734e93d4639c650cd32499e18199
mariampervez <mariam.pervez@nav.no>
2021-11-09 15:44:59 +0100

    Legger til username og password for s3
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -45,3 +45,18 @@ then
     echo "Setter srvusername i eessi-pensjon-journalforing q1 i team namespace"
     export srvusername=$(cat /var/run/secrets/nais.io/srveessi-pensjon-journalforing-q1/username)
 fi
+
+echo "Sjekker eessi_pensjon_journalforing_s3_accesskey"
+if test -f /var/run/secrets/nais.io/appcredentials/s3_accesskey;
+then
+  echo "Setter eessi_pensjon_journalforing_s3_accesskey"
+    export s3_accesskey=$(cat /var/run/secrets/nais.io/appcredentials/s3_accesskey)
+fi
+
+echo "Sjekker eessi_pensjon_journalforing_s3_secretkey"
+if test -f /var/run/secrets/nais.io/appcredentials/s3_secretkey;
+then
+  echo "Setter eessi_pensjon_journalforing_s3_secretkey"
+    export s3_secretkey=$(cat /var/run/secrets/nais.io/appcredentials/s3_secretkey)
+fi
+
