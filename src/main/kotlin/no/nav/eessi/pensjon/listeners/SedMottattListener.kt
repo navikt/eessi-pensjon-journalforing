16995f5a5b55ae4c73145f86f0c93ff3e779a4e1
mariampervez <mariam.pervez@nav.no>
2021-12-14 09:07:21 +0100

    Prodfiks: Hopper over offset 326254, innkommende X050

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedMottattListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedMottattListener.kt
@@ -22,7 +22,7 @@ import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 import java.util.*
-import java.util.concurrent.CountDownLatch
+import java.util.concurrent.*
 import javax.annotation.PostConstruct
 
 @Service
@@ -66,7 +66,7 @@ class SedMottattListener(
                 logger.debug(hendelse)
 
                 //Forsøker med denne en gang til 258088L
-                val offsetToSkip = listOf(38518L,166333L, 195180L, 195186L, 195187L, 195188L, 195449L, 197341L, 197342L, 197343L, 206688L, 118452L, 268237L, 268268L, 268280L, 268281L, 268282L, 291953L, 299754L, 300064L, 300173L, 300204L, 300211L, 300971L, 301837L)
+                val offsetToSkip = listOf(38518L,166333L, 195180L, 195186L, 195187L, 195188L, 195449L, 197341L, 197342L, 197343L, 206688L, 118452L, 268237L, 268268L, 268280L, 268281L, 268282L, 291953L, 299754L, 300064L, 300173L, 300204L, 300211L, 300971L, 301837L, 326254L)
                 try {
                     val offset = cr.offset()
                     if (offset in offsetToSkip) {
