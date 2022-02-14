61987301e4b92774b90a70e65fa04de75df4ef55
Mo Amini <mo.amini@nav.no>
2022-02-14 13:21:39 +0100

    skrur på tokenvalidation

--- src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonJournalforingApplication.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/EessiPensjonJournalforingApplication.kt
@@ -1,11 +1,13 @@
 package no.nav.eessi.pensjon
 
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
 
 @SpringBootApplication
+@EnableJwtTokenValidation
 @EnableOAuth2Client(cacheEnabled = true)
 @EnableRetry
 class EessiPensjonJournalforingApplication
