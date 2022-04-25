0438763af8fec00137009495aab227935e56f955
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-04-25 14:35:48 +0200

    Bytte ut deprekert ErrorHandler

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaErrorHandler.kt
@@ -4,8 +4,8 @@ import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
+import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler
 import org.springframework.kafka.listener.ContainerAwareErrorHandler
-import org.springframework.kafka.listener.ContainerStoppingErrorHandler
 import org.springframework.kafka.listener.MessageListenerContainer
 import org.springframework.stereotype.Component
 import java.io.PrintWriter
@@ -17,7 +17,7 @@ import java.io.StringWriter
 class KafkaErrorHandler : ContainerAwareErrorHandler {
     private val logger = LoggerFactory.getLogger(KafkaErrorHandler::class.java)
 
-    private val stopper = ContainerStoppingErrorHandler()
+    private val stopper = CommonContainerStoppingErrorHandler()
 
     override fun handle(
         thrownException: java.lang.Exception,
@@ -30,7 +30,7 @@ class KafkaErrorHandler : ContainerAwareErrorHandler {
 
         logger.error("En feil oppstod under kafka konsumering av meldinger: \n ${hentMeldinger(records)} \n" +
                 "Stopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
-        stopper.handle(thrownException, records, consumer, container)
+        stopper.handleRemaining(thrownException, records?: emptyList(), consumer, container)
     }
 
     fun hentMeldinger(records: MutableList<ConsumerRecord<*, *>>?): String {
