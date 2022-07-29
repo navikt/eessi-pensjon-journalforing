292114c376e72906a892fdb8220d6c64244fceb3
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-07-29 10:30:01 +0200

    R!! skriver om KafkaLoggingErrorHandler
    legger til vask av 11 sifre i alle
    baserer oss på CommonContainerStoppingErrorHandler
    
    Co-authored-by: Daniel Skarpås <daniel.skarpas@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaStoppingErrorHandler.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaStoppingErrorHandler.kt
@@ -13,23 +13,24 @@ import java.io.StringWriter
 
 @Profile("prod")
 @Component
-class KafkaStoppingErrorHandler : CommonErrorHandler {
+class KafkaStoppingErrorHandler : CommonContainerStoppingErrorHandler() {
     private val logger = LoggerFactory.getLogger(KafkaStoppingErrorHandler::class.java)
-    private val stopper = CommonContainerStoppingErrorHandler()
 
-    override fun handleRecord(
-        thrownException: Exception,
-        record: ConsumerRecord<*, *>,
+    override fun handleRemaining(
+        thrownException: java.lang.Exception,
+        records: MutableList<ConsumerRecord<*, *>>,
         consumer: Consumer<*, *>,
-        container: MessageListenerContainer) {
-
-        val stacktrace = StringWriter()
-        thrownException.printStackTrace(PrintWriter(stacktrace))
-
-        logger.error("En feil oppstod under kafka konsumering av meldinger: \n" + textListingOf(record ) +
-                "\nStopper containeren ! Restart er nødvendig for å fortsette konsumering, $stacktrace")
-        stopper.handleRemaining(thrownException, listOf(record), consumer, container)
+        container: MessageListenerContainer
+    ) {
+        logger.error("En feil oppstod under kafka konsumering av meldinger: \n" + textListingOf(records) +
+                "\nStopper containeren ! Restart er nødvendig for å fortsette konsumering", thrownException)
+        super.handleRemaining(thrownException, records, consumer, container)
     }
-    fun textListingOf(records: ConsumerRecord<*, *>) = "-" .repeat(20) + records.toString()
 
+    fun textListingOf(records: List<ConsumerRecord<*, *>>) =
+        records.joinToString(separator = "\n") {
+            "-" .repeat(20) + "\n" + vask11sifre(it.toString())
+        }
+
+    private fun vask11sifre(tekst: String) = tekst.replace(Regex("""\b\d{11}\b"""), "***")
 }
