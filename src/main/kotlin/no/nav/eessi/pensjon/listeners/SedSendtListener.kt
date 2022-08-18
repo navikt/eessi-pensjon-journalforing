99e24d9b66d9234a764a336793eb54f0c65618fa
Daniel Skarpås <dskarpas@gmail.com>
2022-08-18 12:55:21 +0200

    Revert "f skipper sedmottatt fra Q når vi er i prod"
    This reverts commit 5b3bc14b6d4e2f8eae04f6a866ae2963ad9b1fbe.

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListener.kt
@@ -59,14 +59,8 @@ class SedSendtListener(
                 logger.info("Innkommet sedSendt hendelse i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
                 try {
                     val offset = cr.offset()
+
                     val sedHendelse = SedHendelseModel.fromJson(hendelse)
-
-                    if (profile == "prod" && sedHendelse.avsenderId in listOf("NO:NAVAT05", "NO:NAVAT07")) {
-                        logger.error("Avsender id er ${sedHendelse.avsenderId}. Dette er testdata i produksjon!!!\n$sedHendelse")
-                        acknowledgment.acknowledge()
-                        return@measure
-                    }
-
                     if (GyldigeHendelser.sendt(sedHendelse)) {
                         val bucType = sedHendelse.bucType!!
 
@@ -102,6 +96,7 @@ class SedSendtListener(
                     }
                     acknowledgment.acknowledge()
                     logger.info("Acket sedSendt melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
+                    logger.info("Genererer automatiseringstatistikk")
 
                 } catch (ex: Exception) {
                     logger.error("Noe gikk galt under behandling av sendt SED-hendelse:\n $hendelse \n", ex)
