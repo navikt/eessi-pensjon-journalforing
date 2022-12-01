722a30258df417683f271f640b0edad0b09c800b
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-12-01 13:21:02 +0100

    F - Introduserer IOExceptionRetryInterceptor

--- src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
@@ -3,6 +3,7 @@ package no.nav.eessi.pensjon.config
 import io.micrometer.core.instrument.MeterRegistry
 import no.nav.eessi.pensjon.logging.RequestIdHeaderInterceptor
 import no.nav.eessi.pensjon.metrics.RequestCountInterceptor
+import no.nav.eessi.pensjon.shared.retry.IOExceptionRetryInterceptor
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
@@ -126,30 +127,6 @@ class RestTemplateConfig(
             val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
             request.headers.setBearerAuth(response.accessToken)
             execution.execute(request, body!!)
-        }
-    }
-
-    internal class IOExceptionRetryInterceptor : ClientHttpRequestInterceptor {
-        private val logger = LoggerFactory.getLogger(IOExceptionRetryInterceptor::class.java)
-
-        override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution) =
-            withRetries { execution.execute(request, body) }
-
-        private fun <T> withRetries(maxAttempts: Int = 3, waitTime: Long = 1L, timeUnit: TimeUnit = TimeUnit.SECONDS, func: () -> T): T {
-            var failException: Throwable? = null
-            var count = 0
-            while (count < maxAttempts) {
-                try {
-                    return func.invoke()
-                } catch (ex: IOException) { // IOException - typisk der som det er nettverksissues
-                    count++
-                    logger.warn("Attempt $count failed with ${ex.message} caused by ${ex.cause}")
-                    failException = ex
-                    Thread.sleep(timeUnit.toMillis(waitTime))
-                }
-            }
-            logger.warn("Giving up after $count attempts.")
-            throw failException!!
         }
     }
 
