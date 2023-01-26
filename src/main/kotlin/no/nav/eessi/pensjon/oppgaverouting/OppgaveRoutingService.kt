01a043b45991e30cd1efd4d31a4d78cbfeba9434
Daniel Skarpås <dskarpas@gmail.com>
2023-01-26 14:10:32 +0100

    R - Stykker opp routing i metoder og etter buctype
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/OppgaveRoutingService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/oppgaverouting/OppgaveRoutingService.kt
@@ -1,6 +1,5 @@
 package no.nav.eessi.pensjon.oppgaverouting
 
-import no.nav.eessi.pensjon.eux.model.BucType
 import no.nav.eessi.pensjon.eux.model.BucType.P_BUC_01
 import no.nav.eessi.pensjon.eux.model.BucType.P_BUC_02
 import no.nav.eessi.pensjon.eux.model.BucType.P_BUC_10
@@ -9,7 +8,6 @@ import no.nav.eessi.pensjon.eux.model.buc.SakType.ALDER
 import no.nav.eessi.pensjon.klienter.norg2.Norg2Service
 import no.nav.eessi.pensjon.klienter.norg2.NorgKlientRequest
 import no.nav.eessi.pensjon.models.Enhet
-import no.nav.eessi.pensjon.models.SakInformasjon
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -46,36 +44,44 @@ class OppgaveRoutingService(private val norg2Service: Norg2Service) {
 
         logger.debug("enhet: $enhet")
 
-        if (oppgave.bucType == P_BUC_01) {
-            val norgKlientRequest = NorgKlientRequest(
-                oppgave.harAdressebeskyttelse,
-                oppgave.landkode,
-                oppgave.geografiskTilknytning,
-                oppgave.saktype
-            )
-            return norg2Service.hentArbeidsfordelingEnhet(norgKlientRequest) ?: enhet
+        return when(oppgave.bucType){
+            P_BUC_01 -> routeTilGeografiskTilknytning(oppgave) ?: enhet
+            P_BUC_02 -> {
+                if (lopendeAldersSakINorge(oppgave)) routeTilGeografiskTilknytningMedPerson(oppgave) ?: enhet
+                else enhet
+            }
+            P_BUC_10 -> routeTilGeografiskTilknytningMedPerson(oppgave) ?: enhet
+            else ->  enhet
         }
+    }
 
-        if (erGydligBuc10eller02(oppgave.bucType, oppgave.landkode, oppgave.sakInformasjon)) {
-            logger.debug("Benytter norg2 for buctype: ${oppgave.bucType}")
-            val personRelasjon = oppgave.identifisertPerson?.personRelasjon
-            val norgKlientRequest = NorgKlientRequest(
-                oppgave.harAdressebeskyttelse,
-                oppgave.landkode,
-                oppgave.geografiskTilknytning,
-                oppgave.saktype,
-                personRelasjon
-            )
-            return norg2Service.hentArbeidsfordelingEnhet(norgKlientRequest) ?: enhet
-        }
-        return enhet
+    private fun lopendeAldersSakINorge( oppgave: OppgaveRoutingRequest) =
+        oppgave.landkode == "NOR" && oppgave.sakInformasjon?.sakType == ALDER && oppgave.sakInformasjon.sakStatus == LOPENDE
+
+    private fun routeTilGeografiskTilknytningMedPerson(oppgave: OppgaveRoutingRequest): Enhet? {
+        logger.debug("Benytter norg2 for buctype: ${oppgave.bucType}")
+        val personRelasjon = oppgave.identifisertPerson?.personRelasjon
+        val norgKlientRequest = NorgKlientRequest(
+            oppgave.harAdressebeskyttelse,
+            oppgave.landkode,
+            oppgave.geografiskTilknytning,
+            oppgave.saktype,
+            personRelasjon
+        )
+        return norg2Service.hentArbeidsfordelingEnhet(norgKlientRequest)
+    }
+
+    private fun routeTilGeografiskTilknytning(oppgave: OppgaveRoutingRequest): Enhet? {
+        val norgKlientRequest = NorgKlientRequest(
+            oppgave.harAdressebeskyttelse,
+            oppgave.landkode,
+            oppgave.geografiskTilknytning,
+            oppgave.saktype
+        )
+        return norg2Service.hentArbeidsfordelingEnhet(norgKlientRequest)
     }
 
     private fun finnEnhetFor(oppgave: OppgaveRoutingRequest) =
         EnhetFactory.hentHandlerFor(bucType = oppgave.bucType).finnEnhet(oppgave)
 
-    fun erGydligBuc10eller02(bucType: BucType, landkode: String?, sakInformasjon: SakInformasjon?): Boolean {
-        if (bucType == P_BUC_10) return true
-        return bucType == P_BUC_02 && landkode == "NOR" && sakInformasjon?.sakType == ALDER && sakInformasjon.sakStatus == LOPENDE
-    }
 }
