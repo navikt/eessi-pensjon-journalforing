86bb3f2fcd6cbf303105c916ed67e32775efe9e5
Marius Eriksen <marius.eriksen@nav.no>
2023-01-26 15:08:15 +0100

    R - Slett og rydd opp i kommentarer
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>

--- src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/PersonModel.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/PersonModel.kt
@@ -9,11 +9,11 @@ import java.time.LocalDate
 data class IdentifisertPerson(
     val aktoerId: String,                               //fra PDL
     val personNavn: String?,                            //fra PDL
-    val landkode: String?,         //fra PDL
-    val geografiskTilknytning: String?,                              //fra PDL
-    val personRelasjon: SEDPersonRelasjon,                 //fra PDL
-    val fodselsdato: String? = null,              //innhenting fra FnrHelper og SED
-    var personListe: List<IdentifisertPerson>? = null   //fra PDL){}
+    val landkode: String?,                              //fra PDL
+    val geografiskTilknytning: String?,                 //fra PDL
+    val personRelasjon: SEDPersonRelasjon,              //fra PDL
+    val fodselsdato: String? = null,                    //innhenting fra FnrHelper og SED
+    var personListe: List<IdentifisertPerson>? = null   //fra PDL
 ) {
     override fun toString(): String {
         return "IdentifisertPerson(aktoerId='$aktoerId', personNavn=$personNavn, landkode=$landkode, geografiskTilknytning=$geografiskTilknytning, personRelasjon=$personRelasjon)"
@@ -31,10 +31,7 @@ data class SEDPersonRelasjon(
     val rinaDocumentId: String
 ) {
     fun isFnrDnrSinFdatoLikSedFdato(): Boolean {
-        //fdato == null and return true validation allow fnr
-        //fdato == null and return false validation fail
         if (fdato == null) return true
-
         return fnr?.getBirthDate() == fdato
     }
 
