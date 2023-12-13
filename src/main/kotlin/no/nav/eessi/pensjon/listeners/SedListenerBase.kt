520b5714af96e6d17778feec21a517963ff41183
Daniel Skarpås <dskarpas@gmail.com>
2023-12-13 14:36:21 +0100

    R - Trekker ut felles logikk for uthenting av pensjonsinformasjon og saktype

---
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListenerBase.kt
@@ -0,0 +1,53 @@
+package no.nav.eessi.pensjon.listeners
+import no.nav.eessi.pensjon.eux.model.BucType
+import no.nav.eessi.pensjon.eux.model.buc.SakStatus.AVSLUTTET
+import no.nav.eessi.pensjon.eux.model.buc.SakType
+import no.nav.eessi.pensjon.eux.model.buc.SakType.GJENLEV
+import no.nav.eessi.pensjon.eux.model.buc.SakType.UFOREP
+import no.nav.eessi.pensjon.eux.model.sed.SED
+import no.nav.eessi.pensjon.klienter.fagmodul.FagmodulService
+import no.nav.eessi.pensjon.klienter.pesys.BestemSakService
+import no.nav.eessi.pensjon.oppgaverouting.SakInformasjon
+import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentifisertPerson
+import org.slf4j.LoggerFactory
+
+open class SedListenerBase(
+    private val fagmodulService: FagmodulService,
+    private val bestemSakService: BestemSakService
+) {
+
+    private val logger = LoggerFactory.getLogger(SedMottattListener::class.java)
+
+    /**
+     * Velger saktype fra enten bestemSak eller pensjonsinformasjon der det foreligger.
+     */
+    protected fun pensjonSakInformasjon(identifisertPerson: IdentifisertPerson?, bucType: BucType, saktypeFraSed: SakType?, alleSedIBuc: List<SED>): SakInformasjon? {
+
+        val aktoerId = identifisertPerson?.aktoerId ?: return null
+            .also { logger.info("IdentifisertPerson mangler aktørId. Ikke i stand til å hente ut saktype fra bestemsak eller pensjonsinformasjon") }
+
+        fagmodulService.hentPensjonSakFraPesys(aktoerId, alleSedIBuc).let { pensjonsinformasjon ->
+            if (pensjonsinformasjon?.sakType != null) {
+                logger.info("Velger sakType ${pensjonsinformasjon.sakType} fra pensjonsinformasjon, for sakid: ${pensjonsinformasjon.sakId}")
+                return pensjonsinformasjon
+            }
+        }
+        bestemSakService.hentSakInformasjonViaBestemSak(aktoerId, bucType, saktypeFraSed, identifisertPerson).let {
+            if (it?.sakType != null) {
+                logger.info("Velger sakType ${it.sakType} fra bestemsak, for sak med sakid: ${it.sakId}")
+                return it
+            }
+        }
+        logger.info("Finner ingen sakType(fra bestemsak og pensjonsinformasjon) returnerer null.")
+        return null
+    }
+
+    //TODO: Kan vi vurdere alle bucer som har mulighet for gjenlevende på samme måte som P_BUC_10 her?
+
+    protected fun populerSaktype(saktypeFraSED: SakType?, sakInformasjon: SakInformasjon?, bucType: BucType): SakType? {
+        if (bucType == BucType.P_BUC_02 && sakInformasjon != null && sakInformasjon.sakType == UFOREP && sakInformasjon.sakStatus == AVSLUTTET) return null
+        else if (bucType == BucType.P_BUC_10 && saktypeFraSED == GJENLEV) return sakInformasjon?.sakType ?: saktypeFraSED
+        else if (saktypeFraSED != null) return saktypeFraSED
+        return sakInformasjon?.sakType
+    }
+}
--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedMottattListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedMottattListener.kt
@@ -1,23 +1,16 @@
 package no.nav.eessi.pensjon.listeners
 
 import no.nav.eessi.pensjon.buc.EuxService
-import no.nav.eessi.pensjon.eux.model.BucType
-import no.nav.eessi.pensjon.eux.model.BucType.*
+import no.nav.eessi.pensjon.eux.model.BucType.P_BUC_10
+import no.nav.eessi.pensjon.eux.model.BucType.R_BUC_02
 import no.nav.eessi.pensjon.eux.model.SedHendelse
-import no.nav.eessi.pensjon.eux.model.buc.SakStatus.AVSLUTTET
-import no.nav.eessi.pensjon.eux.model.buc.SakType
-import no.nav.eessi.pensjon.eux.model.buc.SakType.GJENLEV
-import no.nav.eessi.pensjon.eux.model.buc.SakType.UFOREP
-import no.nav.eessi.pensjon.eux.model.sed.SED
 import no.nav.eessi.pensjon.journalforing.JournalforingService
 import no.nav.eessi.pensjon.klienter.fagmodul.FagmodulService
 import no.nav.eessi.pensjon.klienter.pesys.BestemSakService
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.eessi.pensjon.oppgaverouting.HendelseType.MOTTATT
-import no.nav.eessi.pensjon.oppgaverouting.SakInformasjon
 import no.nav.eessi.pensjon.personidentifisering.PersonidentifiseringService
 import no.nav.eessi.pensjon.personidentifisering.relasjoner.RelasjonsHandler
-import no.nav.eessi.pensjon.personoppslag.pdl.model.IdentifisertPerson
 import no.nav.eessi.pensjon.utils.mapAnyToJsonWithoutSensitiveData
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
@@ -31,7 +24,7 @@ import java.util.*
 import java.util.concurrent.CountDownLatch
 
 @Service
-class SedMottattListener(
+class SedMottattListener (
     private val journalforingService: JournalforingService,
     private val personidentifiseringService: PersonidentifiseringService,
     private val euxService: EuxService,
@@ -39,7 +32,7 @@ class SedMottattListener(
     private val bestemSakService: BestemSakService,
     @Value("\${SPRING_PROFILES_ACTIVE}") private val profile: String,
     @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()
-) {
+) : SedListenerBase(fagmodulService, bestemSakService) {
 
     private val logger = LoggerFactory.getLogger(SedMottattListener::class.java)
 
@@ -110,7 +103,7 @@ class SedMottattListener(
                             val alleSedIBucList = alleSedMedGyldigStatus.flatMap { (_, sed) -> listOf(sed) }
                             val fdato = personidentifiseringService.hentFodselsDato(identifisertPerson, alleSedIBucList, kansellerteSeder)
                             val saktypeFraSed = euxService.hentSaktypeType(sedHendelse, alleSedIBucList).takeIf {bucType == P_BUC_10 || bucType  == R_BUC_02 }
-                            val sakInformasjon = pensjonSakInformasjonMottatt(identifisertPerson, bucType, saktypeFraSed, alleSedIBucList)
+                            val sakInformasjon = pensjonSakInformasjon(identifisertPerson, bucType, saktypeFraSed, alleSedIBucList)
                             val saktype = populerSaktype(saktypeFraSed, sakInformasjon, bucType)
 
                             val currentSed =
@@ -141,55 +134,6 @@ class SedMottattListener(
             }
         }
     }
-
-    private fun pensjonSakInformasjonMottatt(identifisertPerson: IdentifisertPerson?, bucType: BucType, saktypeFraSed: SakType?, alleSedIBuc: List<SED>): SakInformasjon? {
-
-        val aktoerId = identifisertPerson?.aktoerId ?: return null
-            .also { logger.info("IdentifisertPerson mangler aktørId. Ikke i stand til å hente ut saktype fra bestemsak eller pensjonsinformasjon") }
-
-        fagmodulService.hentPensjonSakFraPesys(aktoerId, alleSedIBuc).let { pensjonsinformasjon ->
-            if (pensjonsinformasjon?.sakType != null) {
-                logger.info("Velger sakType ${pensjonsinformasjon.sakType} fra pensjonsinformasjon, for sakid: ${pensjonsinformasjon.sakId}")
-                return pensjonsinformasjon
-            }
-        }
-        bestemSakService.hentSakInformasjonViaBestemSak(aktoerId, bucType, saktypeFraSed, identifisertPerson).let {
-            if (it?.sakType != null) {
-                logger.info("Velger sakType ${it.sakType} fra bestemsak, for sak med sakid: ${it.sakId}")
-                return it
-            }
-        }
-        logger.info("Finner ingen sakType(fra bestemsak og pensjonsinformasjon) returnerer null.")
-        return null
-    }
-
-    //TODO: Kan vi vurdere alle bucer som har mulighet for gjenlevende på samme måte som P_BUC_10 her?
-    private fun populerSaktype(saktypeFraSED: SakType?, sakInformasjon: SakInformasjon?, bucType: BucType): SakType? {
-        if (bucType == P_BUC_02 && sakInformasjon != null && sakInformasjon.sakType == UFOREP && sakInformasjon.sakStatus == AVSLUTTET) return null
-        else if (bucType == P_BUC_10 && saktypeFraSED == GJENLEV) return sakInformasjon?.sakType ?: saktypeFraSED
-        else if (saktypeFraSED != null) return saktypeFraSED
-        return sakInformasjon?.sakType
-    }
-
-    /**
-     * Ikke slett funksjonene under før vi har et bedre opplegg for tilbakestilling av topic.
-     * Se jira-sak: EP-968
-     **/
-
-//   @KafkaListener(
-//           containerFactory = "sedKafkaListenerContainerFactory",
-//           groupId = "\${kafka.sedMottatt.groupid}-recovery",
-//           topicPartitions = [TopicPartition(topic = "\${kafka.sedMottatt.topic}",
-//           partitionOffsets = [PartitionOffset(partition = "0", initialOffset = "745823")])])
-//   fun recoverConsumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-//       if (cr.offset() in listOf<Long>(745823, 745854, 748455, 748872)){
-//           logger.info("Behandler sedMottatt offset: ${cr.offset()}")
-//           consumeSedMottatt(hendelse, cr, acknowledgment)
-//       } else {
-//           throw java.lang.RuntimeException()
-//       }
-//   }
-
 }
 
 internal class SedMottattRuntimeException(cause: Throwable) : RuntimeException(cause)
