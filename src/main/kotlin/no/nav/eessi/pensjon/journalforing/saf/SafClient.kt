33d4202894b4d72bf8477b068d93461967b0d06a
Daniel Skarpås <dskarpas@gmail.com>
2024-02-08 11:40:41 +0100

    F - Sletter retry mulighet for SafClient

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/saf/SafClient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/saf/SafClient.kt
@@ -4,10 +4,7 @@ import no.nav.eessi.pensjon.metrics.MetricsHelper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.*
-import org.springframework.retry.annotation.Backoff
-import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
-import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
 @Component
@@ -26,11 +23,6 @@ class SafClient(private val safGraphQlOidcRestTemplate: RestTemplate,
         HentRinaSakIderFraDokumentMetadata = metricsHelper.init("HentRinaSakIderFraDokumentMetadata", ignoreHttpCodes = listOf(HttpStatus.FORBIDDEN))
     }
 
-    @Retryable(
-        exclude = [HttpClientErrorException.NotFound::class],
-        backoff = Backoff(delayExpression = "@retrySafConfig.initialRetryMillis", delay = 10000L, maxDelay = 100000L, multiplier = 3.0),
-        listeners  = ["retrySafLogger"]
-    )
     fun hentJournalpost(journalpostId: String) {
 
         return HentDokumentInnhold.measure {
