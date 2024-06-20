84589854d203e0ce245771a55ec16695304add8f
mariampervez <mariam.pervez@nav.no>
2024-06-20 13:51:51 +0200

    t - Legger til tiest for oppdatering av journalpost

--- src/test/kotlin/no/nav/eessi/pensjon/klienter/journalpost/JournalpostKlientTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/klienter/journalpost/JournalpostKlientTest.kt
@@ -4,8 +4,14 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import no.nav.eessi.pensjon.journalforing.Bruker
 import no.nav.eessi.pensjon.journalforing.OppdaterDistribusjonsinfoRequest
 import no.nav.eessi.pensjon.journalforing.journalpost.JournalpostKlient
+import no.nav.eessi.pensjon.journalforing.saf.OppdaterJournalpost
+import no.nav.eessi.pensjon.journalforing.saf.SafSak
+import no.nav.eessi.pensjon.models.Behandlingstema
+import no.nav.eessi.pensjon.models.Tema
+import no.nav.eessi.pensjon.oppgaverouting.Enhet
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -28,6 +34,48 @@ internal class JournalpostKlientTest {
     }
 
     @Test
+    fun `Sjekker oppdatering av journalpost`(){
+        val bodySlot = slot<HttpEntity<String>>()
+
+        val journalpostSomSkalOppdateres = OppdaterJournalpost(
+            "12345",
+            emptyList(),
+            SafSak(
+                "12345",
+                "",
+                "PEN," +
+                        "",
+                "",
+                "",
+                ""
+            ),
+            Bruker(
+                "11015698765",
+                "FNR",
+
+                ),
+            Tema.PENSJON,
+            Enhet.PENSJON_UTLAND,
+            Behandlingstema.ALDERSPENSJON
+        )
+
+        val journalPostId = "12345"
+
+        every {
+            mockrestTemplate.exchange(
+                "/journalpost/$journalPostId",
+                HttpMethod.PUT,
+                capture(bodySlot),
+                String::class.java
+            )
+        } returns ResponseEntity(HttpStatus.OK)
+
+        journalpostKlient.oppdaterJournalpost(journalpostSomSkalOppdateres)
+
+        mapJsonToAny<OppdaterJournalpost>(bodySlot.captured.body!!)
+
+    }
+    @Test
     fun `Sjekker opprettelse av journalpost`(){
         val dummyResponse = javaClass.classLoader.getResource("journalpost/opprettJournalpostResponseFalse.json")!!.readText()
 
@@ -46,7 +94,7 @@ internal class JournalpostKlientTest {
     }
 
     @Test
-    fun `Sjekker oppdatering av distribusjonsinfo`(){
+    fun `Sjekker oppdatering av distribusjonsinfo`() {
         val bodySlot = slot<HttpEntity<String>>()
 
         val journalPostId = "123"
