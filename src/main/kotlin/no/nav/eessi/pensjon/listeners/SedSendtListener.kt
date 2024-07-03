6857d0377bbacc8da80bc0e47032ea717fc936c8
Daniel Skarpås <dskarpas@gmail.com>
2024-07-03 10:34:46 +0200

    B - Skipping offset: 294886, X010 feil med saktype

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListener.kt
@@ -46,7 +46,7 @@ class SedSendtListener(
     init {
         consumeOutgoingSed = metricsHelper.init("consumeOutgoingSed")
     }
-    private val offsetsToSkip = listOf<Long>(133722, 143447, 176379, 183457, 183585, 204028)
+    private val offsetsToSkip = listOf<Long>(133722, 143447, 176379, 183457, 183585, 204028, 294886)
 
     @KafkaListener(
         containerFactory = "sedKafkaListenerContainerFactory",
