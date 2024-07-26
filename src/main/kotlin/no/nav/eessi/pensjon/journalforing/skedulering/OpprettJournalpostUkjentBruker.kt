5a0a3ca9b4574d9e4c03c98865c2d1c0abf3065a
mariampervez <mariam.pervez@nav.no>
2024-07-26 09:53:56 +0200

    F - Opprette journalposter og jfr oppgaver på jpDetlajer som har ligget i bucket med ukjent bruker over en dag
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/skedulering/OpprettJournalpostUkjentBruker.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/skedulering/OpprettJournalpostUkjentBruker.kt
@@ -2,6 +2,10 @@ package no.nav.eessi.pensjon.journalforing.skedulering
 
 import no.nav.eessi.pensjon.gcp.GcpStorageService
 import no.nav.eessi.pensjon.journalforing.LagretJournalpostMedSedInfo
+import no.nav.eessi.pensjon.journalforing.journalpost.JournalpostService
+import no.nav.eessi.pensjon.journalforing.opprettoppgave.OppgaveHandler
+import no.nav.eessi.pensjon.journalforing.opprettoppgave.OppgaveMelding
+import no.nav.eessi.pensjon.journalforing.opprettoppgave.OppgaveType
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
@@ -9,7 +13,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class OpprettJournalpostUkjentBruker(
-    private val gcpStorageService: GcpStorageService
+    private val gcpStorageService: GcpStorageService,
+    private val journalpostService: JournalpostService,
+    private val oppgaveHandler: OppgaveHandler,
 ) {
     companion object {
         const val EVERY_TWO_MIN = "0 0/2 * * * ?"
@@ -18,13 +24,34 @@ class OpprettJournalpostUkjentBruker(
 
     @Scheduled(cron = EVERY_TWO_MIN)
     operator fun invoke() {
-        val jp = gcpStorageService.hentGamleRinaSakerMedJPDetlajer(2)
+        val jp = gcpStorageService.hentGamleRinaSakerMedJPDetlajer(1)
 
-        jp?.forEach { mapJsonToAny<LagretJournalpostMedSedInfo>(it).also {
-            logger.info("""Lagret JP hentet fra GCP: 
+        jp?.forEach { journalpostDetaljer ->
+            mapJsonToAny<LagretJournalpostMedSedInfo>(journalpostDetaljer)
+            .also {
+                val response = journalpostService.sendJournalPost(it, "eessipensjon")
+
+                logger.info("""Lagret JP hentet fra GCP: 
                 | sedHendelse: ${it.sedHendelse}
                 | enhet: ${it.journalpostRequest.journalfoerendeEnhet}
-                | tema: ${it.journalpostRequest.tema}""".trimMargin())
+                | tema: ${it.journalpostRequest.tema}""".trimMargin()
+                )
+
+                if(response?.journalpostId != null) {
+                    val melding = OppgaveMelding(
+                        sedType = it.sedHendelse.sedType,
+                        journalpostId = response.journalpostId,
+                        tildeltEnhetsnr = it.journalpostRequest.journalfoerendeEnhet!!,
+                        aktoerId = null,
+                        rinaSakId = it.sedHendelse.rinaSakId,
+                        hendelseType = it.sedHendelseType,
+                        filnavn = null,
+                        oppgaveType = OppgaveType.JOURNALFORING,
+                    ).also { oppgaveMelding ->  logger.info("Opprettet journalforingsoppgave for sak med rinaId: ${oppgaveMelding.rinaSakId}") }
+                    oppgaveHandler.opprettOppgaveMeldingPaaKafkaTopic(melding)
+                } else {
+                    logger.error("Journalpost ikke opprettet")
+                }
         } }
     }
 }
