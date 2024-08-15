c4b092ac59133d9ec6e3eea87dcc411ca35be97a
mariampervez <mariam.pervez@nav.no>
2024-08-15 11:18:50 +0200

    F - Legger pX000Pensjon for P5000, P6000, P7000 og P10000

--- src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/relasjoner/P5000Relasjon.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/personidentifisering/relasjoner/P5000Relasjon.kt
@@ -9,7 +9,7 @@ class P5000Relasjon(private val sed: SED, private val bucType: BucType, val rina
 
     override fun hentRelasjoner(): List<SEDPersonRelasjon> {
         val forsikret = hentForsikretPerson(bestemSaktype(bucType))
-        val gjenlevende = hentRelasjonGjenlevendeFnrHvisFinnes((sed as P5000).p5000Pensjon?.gjenlevende)
+        val gjenlevende = hentRelasjonGjenlevendeFnrHvisFinnes((sed as P5000).pensjon?.gjenlevende)
 
         return gjenlevende.ifEmpty { forsikret }
 
