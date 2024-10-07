f6492d38a78e36705499374fec91f7af38f48277
Daniel Skarpås <dskarpas@gmail.com>
2024-10-07 12:58:07 +0200

    F - Logger sedId for å enklere hente ut info ved rekjøring

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListenerBase.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListenerBase.kt
@@ -96,11 +96,11 @@ abstract class SedListenerBase(
 
     fun behandleHendelse(hendelse: String, sedRetning: HendelseType, acknowledgment: Acknowledgment) {
         val sedHendelse = SedHendelse.fromJson(hendelse)
-        logger.info("buc_export ${sedHendelse.sedId}")
 
         if (profile == "prod" && sedHendelse.avsenderId in TEST_DATA_SENDERS) {
             logger.error("Avsender id er ${sedHendelse.avsenderId}. Dette er testdata i produksjon!!!\n$sedHendelse")
         } else if ((sedRetning == HendelseType.SENDT || sedRetning == HendelseType.MOTTATT) && GyldigeHendelser.sendt(sedHendelse)) {
+            logger.info("Godkjent sed: ${sedHendelse.sedId}, klar for behandling")
             behandleSedHendelse(sedHendelse)
         } else {
             logger.warn("SED: ${sedHendelse.sedType}, ${sedHendelse.rinaSakId} er ikke med i listen over gyldige hendelser")
