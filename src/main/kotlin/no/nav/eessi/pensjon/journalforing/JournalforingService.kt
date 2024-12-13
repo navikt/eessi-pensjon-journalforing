15db59125f2b8ccbb8280db8283059bd8e4643bf
Daniel Skarpås <dskarpas@gmail.com>
2024-12-13 15:47:24 +0100

    F - Legger til sjekk for birth date

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingService.kt
@@ -105,10 +105,10 @@ class JournalforingService(
                 val aktoerId = identifisertPerson?.aktoerId
 
                 //henter først tema da denne også er i bruk for tildeltJoarkEnhet
-                val alder = if(identifisertPerson?.fnr?.erNpid == true){
-                    identifisertPerson.personRelasjon?.alder()
+                val alder = if(identifisertPerson?.fnr?.erNpid == true || identifisertPerson?.fnr?.getBirthDate() == null) {
+                    identifisertPerson?.personRelasjon?.alder()
                 }
-                else identifisertPerson?.personRelasjon?.fnr?.getAge()
+                else identifisertPerson.personRelasjon?.fnr?.getAge()
 
                 val tema = hentTemaService.hentTema(
                     sedHendelse,
