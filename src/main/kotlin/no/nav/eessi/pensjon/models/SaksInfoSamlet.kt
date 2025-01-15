86fb689ff5eb3a13b678e19fbaec81f35334f6e0
mariampervez <mariam.pervez@nav.no>
2025-01-15 10:23:43 +0100

    d - Navnebytte på variabel

--- src/main/kotlin/no/nav/eessi/pensjon/models/SaksInfoSamlet.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/models/SaksInfoSamlet.kt
@@ -5,6 +5,6 @@ import no.nav.eessi.pensjon.oppgaverouting.SakInformasjon
 
 data class SaksInfoSamlet(
     val saksIdFraSed: String? = null,
-    val sakInformasjon: SakInformasjon? = null, // Kommer fra Pesys
-    val saktype: SakType? = null   // Kommer fra SED
+    val sakInformasjonFraPesys: SakInformasjon? = null,
+    val saktypeFraSed: SakType? = null
 )
