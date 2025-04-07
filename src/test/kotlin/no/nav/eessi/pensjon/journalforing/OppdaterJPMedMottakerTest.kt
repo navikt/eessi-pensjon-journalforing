791c7b65c6a69444386793266a491fb3cb7d813f
Daniel Skarpås <dskarpas@gmail.com>
2025-04-07 14:03:11 +0200

    T - Oppdaterer test

--- src/test/kotlin/no/nav/eessi/pensjon/journalforing/OppdaterJPMedMottakerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/journalforing/OppdaterJPMedMottakerTest.kt
@@ -5,6 +5,7 @@ import io.mockk.*
 import no.nav.eessi.pensjon.eux.EuxService
 import no.nav.eessi.pensjon.eux.model.buc.Organisation
 import no.nav.eessi.pensjon.eux.model.buc.Participant
+import no.nav.eessi.pensjon.gcp.GcpStorageService
 import no.nav.eessi.pensjon.journalforing.journalpost.JournalpostKlient
 import no.nav.eessi.pensjon.journalforing.saf.Journalpost
 import no.nav.eessi.pensjon.journalforing.saf.SafClient
@@ -17,25 +18,27 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 
-@Disabled
 class OppdaterJPMedMottakerTest {
 
     private var safClient: SafClient = mockk()
     private var euxService: EuxService = mockk()
     private var journalpostKlient: JournalpostKlient = mockk()
+    private var gcpStorageService: GcpStorageService = mockk()
     private lateinit var oppdaterJPMedMottaker: OppdaterJPMedMottaker
 
     @BeforeEach
     fun setUp() {
-        oppdaterJPMedMottaker = OppdaterJPMedMottaker(safClient, euxService, journalpostKlient, mockk(), "JournalpostIderTest.txt")
+        oppdaterJPMedMottaker = OppdaterJPMedMottaker(safClient, euxService, journalpostKlient, gcpStorageService, "JournalpostIderTest.txt")
 
         every { euxService.hentDeltakereForBuc(any()) } returns deltakere()
         every { journalpostKlient.oppdaterJournalpostMedMottaker(any(), any()) } just Runs
+        every { gcpStorageService.hentIndex() } returns null
 
         val jp = """
             {"journalpostId":"453976326","bruker":{"id":"2964001528817","type":"AKTOERID"},"tittel":"Utgående P6000 - Melding om vedtak","journalposttype":"U","journalstatus":"EKSPEDERT","tema":"PEN","behandlingstema":"ab0254","journalforendeEnhet":"4862","eksternReferanseId":"e05590fc-b93f-48a2-b7c9-2f838692fc68","tilleggsopplysninger":[{"nokkel":"eessi_pensjon_bucid","verdi":"1447240"}],"datoOpprettet":"2025-03-20T10:22:39"}
         """.trimIndent()
         every { safClient.hentJournalpostForJp(any())} returns mapJsonToAny<Journalpost>(jp)
+//        every { safClient.hentJournalpostForJp(any())} returns journalPost andThen journalPost.copy(tilleggsopplysninger = listOf(mapOf("nokkel" to "nyVerdi"), mapOf("verdi" to "1447242")))
 
     }
     @AfterEach
@@ -47,17 +50,17 @@ class OppdaterJPMedMottakerTest {
     @Test
     fun `skal oppdatere alle som ligger `() {
         oppdaterJPMedMottaker.oppdatereHeleSulamitten()
-        verify(exactly = 3) { journalpostKlient.oppdaterJournalpostMedMottaker(any(), avsenderMottker(deltakere().first().organisation!!)) }
+        verify(exactly = 5) { journalpostKlient.oppdaterJournalpostMedMottaker(any(), avsenderMottker(deltakere().first().organisation!!)) }
     }
 
     @Test
     fun `skal hoppe over allerede lagret filer`() {
         OppdaterJPMedMottaker.JournalpostIdFilLager("/tmp/journalpostIderSomGikkBra.txt").leggTil("453976833")
-
+        every { gcpStorageService.hentIndex() } returns "453976833"
         oppdaterJPMedMottaker.oppdatereHeleSulamitten()
 
-        verify(exactly = 2) { euxService.hentDeltakereForBuc(any()) }
-        verify(exactly = 2) { journalpostKlient.oppdaterJournalpostMedMottaker(any(), any()) }
+        verify(exactly = 1) { euxService.hentDeltakereForBuc(any()) }
+        verify(exactly = 3) { journalpostKlient.oppdaterJournalpostMedMottaker(any(), any()) }
     }
 
     fun avsenderMottker(organisation: Organisation) : String {
