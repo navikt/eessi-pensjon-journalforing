6d9d6f6fa55c90b21d32ebc0b99470885f6d2eff
mariampervez <mariam.pervez@nav.no>
2025-04-08 15:04:39 +0200

    t - Parametriserer test for journalforing av R, M og H Bucer

--- src/test/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListenerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/SedSendtListenerTest.kt
@@ -14,6 +14,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.kafka.support.Acknowledgment
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -51,17 +53,49 @@ internal class SedSendtListenerTest {
         verify(exactly = 1) { acknowledgment.acknowledge() }
     }
 
-    @Test
-    fun `Gitt en mottatt P_BUC_01 `() {
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "H_BUC_07, H070",
+            "R_BUC_02, R005",
+            "M_BUC_02, M02",
+            "M_BUC_03a, M03a",
+            "M_BUC_03b, M03bP"
+        ]
+    )
+    fun `Hent ut alle som ligger i gyldigeHendelser listen`(bucType: String, sedType: String) {
+        val enHendelse =
+            """
+                {
+                  "id": 1869,
+                  "sedId": "H070_9498fc46933548518712e4a1d5133113_2",
+                  "sektorKode": "P",
+                  "bucType": "$bucType",
+                  "rinaSakId": "747729177",
+                  "avsenderId": "NO:NAVT003",
+                  "avsenderNavn": "NAVT003",
+                  "avsenderLand": "NO",
+                  "mottakerId": "NO:NAVT007",
+                  "mottakerNavn": "NAV Test 07",
+                  "mottakerLand": "NO",
+                  "rinaDokumentId": "9498fc46933548518712e4a1d5133113",
+                  "rinaDokumentVersjon": "2",
+                  "sedType": "$sedType",
+                  "navBruker": "09035225916"
+                }
+            """.trimIndent()
+
         every { gcpStorageService.gjennyFinnes(any()) } returns true
-        every { gcpStorageService.oppdaterGjennysak(any(), any(), any()) } returns "123546"
+        every { gcpStorageService.oppdaterGjennysak(any(), any(), any()) } returns "747729177"
         every { gcpStorageService.hentFraGjenny(any()) } returns null
-        sedListener.consumeSedSendt(String(Files.readAllBytes(Paths.get("src/test/resources/eux/hendelser/P_BUC_01_P2000.json"))), cr, acknowledgment)
+
+        sedListener.consumeSedSendt((enHendelse), cr, acknowledgment)
 
         verify(exactly = 0) { gcpStorageService.lagre(any(), any()) }
         verify(exactly = 1) { acknowledgment.acknowledge() }
         verify(exactly = 1) { jouralforingService.journalfor(any(), any(), any(), any(), any(), any(), any(), any(), any()) }
     }
+
 
     @Test
     fun `gitt en ugyldig sedHendelse av type R_BUC_02 når sedSendt hendelse konsumeres, skal melding ackes`() {
