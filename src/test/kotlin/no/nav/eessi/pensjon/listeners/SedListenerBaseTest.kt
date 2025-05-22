6c2c6eef430cfc184166f7e37f103e95efc4ee41
Daniel Skarpås <dskarpas@gmail.com>
2025-05-22 09:24:25 +0200

    e - Oppdatering org.springframework:spring-framework-bom og fix for init-test-feil

--- src/test/kotlin/no/nav/eessi/pensjon/listeners/SedListenerBaseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/listeners/SedListenerBaseTest.kt
@@ -55,126 +55,126 @@ class SedListenerBaseTest {
         }
     }
 
-//    @ParameterizedTest
-//    @CsvSource(
-//        "SENDT, P_BUC_10, GJENLEV,22975700, true",
-//        "MOTTATT, P_BUC_10, GJENLEV, 22975700, true",
-//        "MOTTATT, P_BUC_10, GJENLEV, 22975710, false",
-//        "MOTTATT, P_BUC_10, ALDER, 22975710, false",
-//        "MOTTATT, P_BUC_10, ALDER, null, true"
-//    )
-//    fun `hentSaksInformasjonForEessi skal gi advarsel der hvor pesys nr fra pesys ikke stemmer med sed pesys nr 22975710`(
-//        hendelseType: HendelseType,
-//        bucType: BucType,
-//        sakTypeFraSed: SakType?,
-//        sakId: String?,
-//        expectedAdvarsel: Boolean
-//    ) {
-//        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/P8000_pesysId.json")!!.readText())
-//        val alleSedIBucList = listOf<SED>(sed)
-//
-//        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
-//        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
-//        val saksInfo = listOf(SakInformasjon(
-//            sakId = sakId,
-//            sakStatus = SakStatus.LOPENDE,
-//            sakType = SakType.GJENLEV,
-//            saksbehandlendeEnhetId = "NFP_UTLAND_AALESUND",
-//            nyopprettet = false
-//        ))
-//
-//        every { euxService.hentSaktypeType(any(), any()) } returns sakTypeFraSed
-//        every { fagmodulKlient.hentPensjonSaklist(any()) } returns saksInfo
-//
-//        val result = sedListenerBase.hentSaksInformasjonForEessi(
-//            alleSedIBucList,
-//            sedHendelse,
-//            bucType,
-//            identifisertPerson,
-//            hendelseType,
-//            sed
-//        )
-//
-//        assertEquals(expectedAdvarsel, result.advarsel)
-//    }
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        SENDT, P_BUC_10, GJENLEV, 22975700, true
+        MOTTATT, P_BUC_10, GJENLEV, 22975700, true
+        MOTTATT, P_BUC_10, GJENLEV, 22975710, false
+        MOTTATT, P_BUC_10, ALDER, 22975710, false
+        MOTTATT, P_BUC_10, ALDER, null, true"""
+    )
+    fun `hentSaksInformasjonForEessi skal gi advarsel der hvor pesys nr fra pesys ikke stemmer med sed pesys nr 22975710`(
+        hendelseType: HendelseType,
+        bucType: BucType,
+        sakTypeFraSed: SakType?,
+        sakId: String?,
+        expectedAdvarsel: Boolean
+    ) {
+        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/P8000_pesysId.json")!!.readText())
+        val alleSedIBucList = listOf<SED>(sed)
 
-//    @DisplayName("Vurder om pesys sakId er gyldig gitt flere pesys sak id fra Pesys: ")
-//    @ParameterizedTest
-//    @CsvSource(
-//        "22975232, true, true, '22975710;22975232'",
-//        "22223332, false, true, '22975710;22975232'",
-//        ", false, false, '22975710;22975232'"
-//    )
-//    fun testHentSaksInformasjonForEessi(sakIdFraPesys: String?, harSakIdFraSed: Boolean, harPesysSakId: Boolean, pesysIdListe: String) {
-//        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/P8000_flere_pesysId.json")!!.readText())
-//        val alleSedIBucList = listOf(sed)
-//
-//        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
-//        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
-//
-//        every { euxService.hentSaktypeType(any(), any()) } returns SakType.ALDER
-//
-//        val saksInfo = if (!sakIdFraPesys.isNullOrEmpty()) {
-//            listOf(mockk<SakInformasjon>().apply {
-//                every { sakId } returns sakIdFraPesys
-//                every { sakStatus } returns SakStatus.LOPENDE
-//                every { sakType } returns SakType.GJENLEV
-//                every { saksbehandlendeEnhetId } returns "NFP_UTLAND_AALESUND"
-//                every { nyopprettet } returns false
-//            })
-//        } else {
-//            emptyList()
-//        }
-//        every { fagmodulKlient.hentPensjonSaklist(any()) } returns saksInfo
-//
-//        val result = sedListenerBase.hentSaksInformasjonForEessi(
-//            alleSedIBucList,
-//            sedHendelse,
-//            BucType.P_BUC_10,
-//            identifisertPerson,
-//            HendelseType.MOTTATT,
-//            sed
-//        )
-//
-//        val listeAvPesysId = pesysIdListe.split(";")
-//
-//        assertEquals(if (harSakIdFraSed) "22975232" else null, result.saksIdFraSed)
-//        assertEquals(if (harPesysSakId) sakIdFraPesys else null, result.sakInformasjonFraPesys?.sakId)
-//     }
-//
-//    @ParameterizedTest(name = "[{index}] {arguments}")
-//    @CsvSource(useHeadersInDisplayName = true, textBlock = """
-//        ID FRA PENSJON (PESYS), RESULTAT,         SED FILNAVN
-//        '22975232;22975200',    22975232,         P8000_flere_pesysId.json      // sed id matcher pesys; velger første valgte nummer
-//        '22111111;22222222',    null,             P8000_flere_pesysId.json      // ingen match, flere pesys id og flere sed id; ikke nok informasjon til å ta et bra val
-//        '22111111;22222222',    null,             P8000_pesysId.json            // ingen match, flere pesys og enkel sed id; ikke nok informasjon til å ta et bra val
-//        '22975200',             22975200,         P8000_pesysId.json            // ingen match, mem svar (1) fra pesys: velger dette
-//        '22975200',             22975200,         P8000_ingen_pesysId.json""")  // ingen match, mangler id i sed, men svar(1) fra pesys: velger dette
-//    fun `hentSaksInformasjonForEessi hvor det er flere pesysid fra sed og pesys`(sakIdFraPesys: String?, valgtPesysId: String?, sedFil: String) {
-//        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/$sedFil")!!.readText())
-//        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
-//        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
-//
-//        every { euxService.hentSaktypeType(any(), any()) } returns SakType.ALDER
-//        every { fagmodulKlient.hentPensjonSaklist(any()) } returns sakIdFraPesys?.split(";")?.map { pesysId ->
-//            spyk(SakInformasjon(
-//                sakId = pesysId,
-//                sakStatus = SakStatus.LOPENDE,
-//                sakType = SakType.GJENLEV,
-//                saksbehandlendeEnhetId = "NFP_UTLAND_AALESUND",
-//                nyopprettet = false
-//            ))
-//        }!!
-//
-//        val result = sedListenerBase.hentSaksInformasjonForEessi(
-//            listOf(sed),
-//            sedHendelse,
-//            BucType.P_BUC_10,
-//            identifisertPerson,
-//            HendelseType.MOTTATT,
-//            sed
-//        )
-//        assertEquals(valgtPesysId?.takeIf { it != "null" }, result.sakInformasjonFraPesys?.sakId)
-//    }
+        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
+        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
+        val saksInfo = listOf(SakInformasjon(
+            sakId = sakId,
+            sakStatus = SakStatus.LOPENDE,
+            sakType = SakType.GJENLEV,
+            saksbehandlendeEnhetId = "NFP_UTLAND_AALESUND",
+            nyopprettet = false
+        ))
+
+        every { euxService.hentSaktypeType(any(), any()) } returns sakTypeFraSed
+        every { fagmodulKlient.hentPensjonSaklist(any()) } returns saksInfo
+
+        val result = sedListenerBase.hentSaksInformasjonForEessi(
+            alleSedIBucList,
+            sedHendelse,
+            bucType,
+            identifisertPerson,
+            hendelseType,
+            sed
+        )
+
+        assertEquals(expectedAdvarsel, result.advarsel)
+    }
+
+    @DisplayName("Vurder om pesys sakId er gyldig gitt flere pesys sak id fra Pesys: ")
+    @ParameterizedTest
+    @CsvSource(
+        "22975232, true, true, '22975710;22975232'",
+        "22223332, false, true, '22975710;22975232'",
+        ", false, false, '22975710;22975232'"
+    )
+    fun testHentSaksInformasjonForEessi(sakIdFraPesys: String?, harSakIdFraSed: Boolean, harPesysSakId: Boolean, pesysIdListe: String) {
+        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/P8000_flere_pesysId.json")!!.readText())
+        val alleSedIBucList = listOf(sed)
+
+        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
+        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
+
+        every { euxService.hentSaktypeType(any(), any()) } returns SakType.ALDER
+
+        val saksInfo = if (!sakIdFraPesys.isNullOrEmpty()) {
+            listOf(mockk<SakInformasjon>().apply {
+                every { sakId } returns sakIdFraPesys
+                every { sakStatus } returns SakStatus.LOPENDE
+                every { sakType } returns SakType.GJENLEV
+                every { saksbehandlendeEnhetId } returns "NFP_UTLAND_AALESUND"
+                every { nyopprettet } returns false
+            })
+        } else {
+            emptyList()
+        }
+        every { fagmodulKlient.hentPensjonSaklist(any()) } returns saksInfo
+
+        val result = sedListenerBase.hentSaksInformasjonForEessi(
+            alleSedIBucList,
+            sedHendelse,
+            BucType.P_BUC_10,
+            identifisertPerson,
+            HendelseType.MOTTATT,
+            sed
+        )
+
+        val listeAvPesysId = pesysIdListe.split(";")
+
+        assertEquals(if (harSakIdFraSed) "22975232" else null, result.saksIdFraSed)
+        assertEquals(if (harPesysSakId) sakIdFraPesys else null, result.sakInformasjonFraPesys?.sakId)
+     }
+
+    @ParameterizedTest(name = "[{index}] {arguments}")
+    @CsvSource(useHeadersInDisplayName = true, textBlock = """
+        ID FRA PENSJON (PESYS), RESULTAT,         SED FILNAVN
+        '22975232;22975200',    22975232,         P8000_flere_pesysId.json      // sed id matcher pesys; velger første valgte nummer
+        '22111111;22222222',    null,             P8000_flere_pesysId.json      // ingen match, flere pesys id og flere sed id; ikke nok informasjon til å ta et bra val
+        '22111111;22222222',    null,             P8000_pesysId.json            // ingen match, flere pesys og enkel sed id; ikke nok informasjon til å ta et bra val
+        '22975200',             22975200,         P8000_pesysId.json            // ingen match, mem svar (1) fra pesys: velger dette
+        '22975200',             22975200,         P8000_ingen_pesysId.json""")  // ingen match, mangler id i sed, men svar(1) fra pesys: velger dette
+    fun `hentSaksInformasjonForEessi hvor det er flere pesysid fra sed og pesys`(sakIdFraPesys: String?, valgtPesysId: String?, sedFil: String) {
+        val sed = mapJsonToAny<P8000>(javaClass.getResource("/sed/$sedFil")!!.readText())
+        val sedHendelse = mockk<SedHendelse> { every { rinaSakId } returns "12345" }
+        val identifisertPerson = mockk<IdentifisertPDLPerson> { every { aktoerId } returns "123456799" }
+
+        every { euxService.hentSaktypeType(any(), any()) } returns SakType.ALDER
+        every { fagmodulKlient.hentPensjonSaklist(any()) } returns sakIdFraPesys?.split(";")?.map { pesysId ->
+            spyk(SakInformasjon(
+                sakId = pesysId,
+                sakStatus = SakStatus.LOPENDE,
+                sakType = SakType.GJENLEV,
+                saksbehandlendeEnhetId = "NFP_UTLAND_AALESUND",
+                nyopprettet = false
+            ))
+        }!!
+
+        val result = sedListenerBase.hentSaksInformasjonForEessi(
+            listOf(sed),
+            sedHendelse,
+            BucType.P_BUC_10,
+            identifisertPerson,
+            HendelseType.MOTTATT,
+            sed
+        )
+        assertEquals(valgtPesysId?.takeIf { it != "null" }, result.sakInformasjonFraPesys?.sakId)
+    }
 
 }
