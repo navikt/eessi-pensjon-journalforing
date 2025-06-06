841eec36b3a08eef6c972e02378963711bd6a69f
Daniel Skarpås <dskarpas@gmail.com>
2025-06-06 14:14:13 +0200

    d - Logger info, ikke warning for gjenny sak

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/JournalforingService.kt
@@ -260,7 +260,7 @@ class JournalforingService(
                         throw Exception("Utgående P_BUC_02, oppretter IKKE journalføringsoppgave, slett ${sedHendelse.rinaSakId}")
                     }
                 }
-                logger.warn("Utgående P_BUC_02 og SENDT, er gjennysak, oppretter oppgave")
+                logger.info("Utgående P_BUC_02 og SENDT, er gjennysak, oppretter oppgave")
             }
             val melding = OppgaveMelding(
                 sedHendelse.sedType,
