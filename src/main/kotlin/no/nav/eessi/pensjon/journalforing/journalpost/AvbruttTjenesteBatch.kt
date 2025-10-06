44e6430f62ee644ae8ea2995d158664727ecb0f0
Daniel Skarpås <dskarpas@gmail.com>
2025-10-06 10:09:59 +0200

    F - Setter avbrutt for resten av journalpostene fra https://nav-it.slack.com/archives/CF50BDF7T/p1759732842578479

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/journalpost/AvbruttTjenesteBatch.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/journalpost/AvbruttTjenesteBatch.kt
@@ -14,12 +14,38 @@ class AvbruttTjenesteBatch (
 ) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(AvbruttTjenesteBatch::class.java) }
 
-    @Scheduled(cron = "0 57 9 * * *")
+    @Scheduled(cron = "0 25 10 * * *")
     fun settJournalposterTilAvbrutt() {
         logger.info("Starter batch for å sette journalposter til avbrutt")
-        val names = listOf("673823640")
+        val names = listOf(
+            "673823639",
+            "673823580",
+            "675636958",
+            "675878639",
+            "677455545",
+            "678218560",
+            "673000052",
+            "673823645",
+            "673000054",
+            "679937129",
+            "679937076",
+            "679937130",
+            "679505238",
+            "679505249",
+            "681655379",
+            "674374032",
+            "553465180",
+            "619695191",
+            "645153145",
+            "643414733",
+            "679937118",
+            "673823582",
+            "673000053",
+            "673000049"
+        )
         names.forEach { name ->
             journalpostKlient.oppdaterJournalpostMedAvbrutt(name)
+            Thread.sleep(3000)
         }
     }
 }
