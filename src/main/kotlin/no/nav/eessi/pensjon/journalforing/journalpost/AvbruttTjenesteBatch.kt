9f6387e29e0971191ede6092980d04c173136dc3
Daniel Skarpås <dskarpas@gmail.com>
2025-10-06 09:46:32 +0200

    F - Starter batch 9: 57

--- src/main/kotlin/no/nav/eessi/pensjon/journalforing/journalpost/AvbruttTjenesteBatch.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/journalforing/journalpost/AvbruttTjenesteBatch.kt
@@ -1,5 +1,7 @@
 package no.nav.eessi.pensjon.journalforing.journalpost
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Service
 class AvbruttTjenesteBatch (
     private val journalpostKlient: JournalpostKlient
 ) {
-    @Scheduled(cron = "0 45 9 * * *")
+    private val logger: Logger by lazy { LoggerFactory.getLogger(AvbruttTjenesteBatch::class.java) }
+
+    @Scheduled(cron = "0 57 9 * * *")
     fun settJournalposterTilAvbrutt() {
+        logger.info("Starter batch for å sette journalposter til avbrutt")
         val names = listOf("673823640")
         names.forEach { name ->
             journalpostKlient.oppdaterJournalpostMedAvbrutt(name)
