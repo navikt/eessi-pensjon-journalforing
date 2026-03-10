7b2fc8146197a47067f117163f977ba3f120b304
Daniel Skarpås <dskarpas@gmail.com>
2026-03-10 14:05:23 +0100

    F - Legger til ny heap / jvm

--- init-scripts/ep-jvm-tuning.sh
+++ init-scripts/ep-jvm-tuning.sh
@@ -4,7 +4,7 @@
 # (an alternative is to set Xmx and Xms)
 # 75 is a good starting point, but you might set it even higher
 
-DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:MaxRAMPercentage=80"
+#DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:MaxRAMPercentage=80"
 
 # No of processors, check what the JVM sees using:
 #   kubectl exec -c <container> <pod> -- bash -c 'echo "System.out.println(Runtime.getRuntime().availableProcessors());"|jshell -s - 2>/dev/null' 2>/dev/null
@@ -21,9 +21,9 @@ DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:ActiveProcessorCount=2"
 # On heaps < 2GB choose Parallel GC. On heaps 2-4GB you might get better throughput with G1GC, but ParallelGC is usually better
 # DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseParallelGC"
 # On heaps over 4GB choose G1GC
- DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
+# DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
 # However ... it is easier to see actual memory need using G1GC as it releases memory faster, and keeps memory use more in check (our experience)
 
-DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
-
+#DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
+DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC -XX:InitialRAMPercentage=25.0 -XX:MaxRAMPercentage=70.0 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp"
 export DEFAULT_JVM_OPTS
