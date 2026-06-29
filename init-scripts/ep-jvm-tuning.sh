9579dd060a06d98d1b128df57d66c0c95e0df76a
Daniel Skarpås <dskarpas@gmail.com>
2026-06-29 10:42:54 +0200

    R - Setter tilbake egen konfig for heaps (ihht template) (epd)

--- init-scripts/ep-jvm-tuning.sh
+++ init-scripts/ep-jvm-tuning.sh
@@ -4,7 +4,7 @@
 # (an alternative is to set Xmx and Xms)
 # 75 is a good starting point, but you might set it even higher
 
-#DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:MaxRAMPercentage=80"
+DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:MaxRAMPercentage=80"
 
 # No of processors, check what the JVM sees using:
 #   kubectl exec -c <container> <pod> -- bash -c 'echo "System.out.println(Runtime.getRuntime().availableProcessors());"|jshell -s - 2>/dev/null' 2>/dev/null
@@ -24,6 +24,6 @@ DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:ActiveProcessorCount=2"
 # DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
 # However ... it is easier to see actual memory need using G1GC as it releases memory faster, and keeps memory use more in check (our experience)
 
-DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseZGC -Xmx1g -XX:SoftMaxHeapSize=1g -XX:+ZGenerational"
-#  DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC -XX:InitialRAMPercentage=25.0 -XX:MaxRAMPercentage=70.0 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp"
+DEFAULT_JVM_OPTS="${DEFAULT_JVM_OPTS} -XX:+UseG1GC"
+
 export DEFAULT_JVM_OPTS
